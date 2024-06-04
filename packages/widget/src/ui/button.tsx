98ff68dd4a25b922d42c86b8598a17944c391eaf
Nur Fikri <codingki@gmail.com>
2024-06-04 11:59:05 +0700

    update tailwind config

--- packages/widget/src/ui/button.tsx
+++ packages/widget/src/ui/button.tsx
@@ -1,7 +1,7 @@
 export const WidgetButton = () => {
   return (
     <div className="flex flex-col font-jost">
-      <p className="text-green-500 ">this component using tailwind</p>
+      <p className="text-blackA8 ">this component using tailwind</p>
       <p>test</p>
       <button className="bg-blue-700 text-yellow-500 font-bold">
         Hello world from tailwind
