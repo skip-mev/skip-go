73092fb912505db3b4cc576bb266cfb3cb415ad0
Nur Fikri <codingki@gmail.com>
2024-06-04 11:51:01 +0700

    add font

--- packages/widget/src/ui/button.tsx
+++ packages/widget/src/ui/button.tsx
@@ -1,8 +1,10 @@
 export const WidgetButton = () => {
   return (
-    <div className="flex flex-col">
-      <button className="bg-blue-700 text-yellow-500">
-        Hello world tailwind
+    <div className="flex flex-col font-jost">
+      <p className="text-green-500 ">this component using tailwind</p>
+      <p>test</p>
+      <button className="bg-blue-700 text-yellow-500 font-bold">
+        Hello world from tailwind
       </button>
     </div>
   );
