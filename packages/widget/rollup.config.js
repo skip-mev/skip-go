b37d45137e7b41023398102117a291371ab7509d
Nur Fikri <codingki@gmail.com>
2024-06-15 07:00:40 +0700

    feat: modify deps

--- packages/widget/rollup.config.js
+++ packages/widget/rollup.config.js
@@ -7,16 +7,7 @@ import packageJson from './package.json';
 export default [
   {
     input: ['./src/index.ts'],
-    external: [
-      'react',
-      'react-dom',
-      'wagmi',
-      '@wagmi/core',
-      '@cosmos-kit/*',
-      '@solana/*',
-      '@cosmjs/*',
-      '@skip-router/core',
-    ],
+    external: ['react', 'react-dom'],
     output: {
       file: packageJson.exports.import,
       format: 'esm',
