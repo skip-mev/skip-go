952f1aaffc24c8a59a4793c0a7d7428feb16e5fb
todd kao <todd.kakao@gmail.com>
2024-07-03 14:22:59 -0400

    fix build

--- packages/widget/src/ui/WebComponent.tsx
+++ packages/widget/src/ui/WebComponent.tsx
@@ -1,20 +1,20 @@
-import { SwapWidget, SwapWidgetProps } from "./index";
-import { SwapWidgetProvider } from "../provider";
+import { SwapWidget, SwapWidgetProps } from './index';
+import { SwapWidgetProvider } from '../provider';
 
 type Stringify<T> = {
   [K in keyof T]: string;
 };
 
 type SwapWidgetWebComponentProps = Stringify<SwapWidgetProps> & {
-  children: any;
-}
+  children?: any;
+};
 
 const WidgetWithProvider = (props: SwapWidgetProps) => {
   return (
     <SwapWidgetProvider>
       <SwapWidget {...props} />
     </SwapWidgetProvider>
-  )
+  );
 };
 
 const WEB_COMPONENT_NAME = 'skip-widget';
@@ -23,26 +23,28 @@ let initialized = false;
 
 export const initializeSwapWidget = () => {
   if (!initialized && typeof window !== 'undefined') {
-    import('@r2wc/react-to-web-component').then(({ default: ReactToWebComponent }) => {
-      const WebComponent = ReactToWebComponent(WidgetWithProvider, {
-        props: {
-          colors: 'json',
-          className: 'string',
-          style: 'string',
-          settings: 'json',
-          onlyTestnet: 'json',
-          defaultRoute: 'json',
-          routeConfig: 'json',
-        }
-      });
+    import('@r2wc/react-to-web-component').then(
+      ({ default: ReactToWebComponent }) => {
+        const WebComponent = ReactToWebComponent(WidgetWithProvider, {
+          props: {
+            colors: 'json',
+            className: 'string',
+            style: 'string',
+            settings: 'json',
+            onlyTestnet: 'json',
+            defaultRoute: 'json',
+            routeConfig: 'json',
+          },
+        });
 
-      if (!customElements.get(WEB_COMPONENT_NAME)) {
-        customElements.define(WEB_COMPONENT_NAME, WebComponent);
+        if (!customElements.get(WEB_COMPONENT_NAME)) {
+          customElements.define(WEB_COMPONENT_NAME, WebComponent);
+        }
+        initialized = true;
       }
-      initialized = true;
-    });
+    );
   }
-}
+};
 
 declare global {
   namespace JSX {
