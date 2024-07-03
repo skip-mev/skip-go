6c5a931860678016574d40f8e56db8ee48603e1b
todd kao <todd.kakao@gmail.com>
2024-07-03 14:13:54 -0400

    WIP adding themes, Add styled-components

---
+++ packages/widget/src/ui/theme.ts
@@ -0,0 +1,25 @@
+import 'styled-components';
+
+export const defaultTheme = {
+  primary: {
+    backgroundColor: 'yellow',
+    textColor: 'black',
+  },
+  secondary: {
+    backgroundColor: '',
+    textColor: '',
+  },
+};
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    primary: {
+      backgroundColor: string;
+      textColor: string;
+    };
+    secondary: {
+      backgroundColor: string;
+      textColor: string;
+    };
+  }
+}
