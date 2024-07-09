1a214f54daf920e0633359921323b3ba9ad3ebbe
todd kao <todd.kakao@gmail.com>
2024-07-09 11:03:26 -0400

    Testing change tsup.config.ts target

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -1,16 +1,16 @@
-import type { Options } from "tsup";
-import { defineConfig } from "tsup";
+import type { Options } from 'tsup';
+import { defineConfig } from 'tsup';
 
 const defaultOptions: Options = {
   cjsInterop: true,
   clean: true,
   dts: true,
-  format: ["cjs"],
+  format: ['cjs'],
   minify: false,
   shims: true,
   splitting: true,
   treeshake: true,
-  tsconfig: "./tsconfig.build.json",
+  tsconfig: './tsconfig.build.json',
 };
 
 export default defineConfig(async ({ watch }) => {
@@ -19,19 +19,20 @@ export default defineConfig(async ({ watch }) => {
       ...defaultOptions,
       clean: !watch,
       entry: {
-        index: "src/index.ts",
-        parser: "src/parser.ts",
-        transactions: "src/transactions.ts",
-        types: "src/types/index.ts",
+        index: 'src/index.ts',
+        parser: 'src/parser.ts',
+        transactions: 'src/transactions.ts',
+        types: 'src/types/index.ts',
       },
       external: [
         /^@cosmjs\/.*/,
         /^@injectivelabs\/.*/,
         /^@protobufjs\/.*/,
-        "long",
-        "protobufjs",
+        'long',
+        'protobufjs',
         //
       ],
+      target: ['es2020'],
     },
   ];
 });
--- packages/core/vitest.config.mjs
+++ packages/core/vitest.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vitest/config';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
   test: {
@@ -15,5 +14,4 @@ export default defineConfig({
     },
     globals: true,
   },
-  plugins: [nodePolyfills()],
 });
