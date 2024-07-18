93b213baacac31fd0f0ee67a466abc396f1eddee
todd kao <todd.kakao@gmail.com>
2024-07-18 11:11:27 -0400

    Export swap-widget-without-providers, Add example

--- packages/widget/src/index.ts
+++ packages/widget/src/index.ts
@@ -1,5 +1,10 @@
 export { SwapWidgetProvider } from './provider';
-export { SwapWidget, SwapWidgetProps } from './ui';
+export {
+  SwapWidget,
+  SwapWidgetProps,
+  SwapWidgetWithoutProviders,
+  SwapWidgetWithoutProvidersProps,
+} from './ui';
 export { initializeSwapWidget } from './ui/WebComponent';
 export { useAssets } from './provider/assets';
 export { useChains, useChainByID } from './hooks/use-chains';
