c0d6b761ada05995a51152f7e9fa5f4ba08d7203
todd kao <todd.kakao@gmail.com>
2024-07-19 16:50:27 -0400

    Fix provider issue by using react-shadow-scope

--- packages/widget/src/ui/WithStyledShadowDom.tsx
+++ packages/widget/src/ui/WithStyledShadowDom.tsx
@@ -1,70 +1,49 @@
-import { ReactNode, useCallback, useEffect, useRef, useState } from 'react';
-import { createRoot } from 'react-dom/client';
+import { ReactNode, useCallback, useEffect, useState } from 'react';
 import { StyleSheetManager } from 'styled-components';
 import shadowDomStyles from '../styles/shadowDomStyles.css';
 import toastStyles from '../styles/toastStyles.css';
 import cssReset from '../styles/cssReset.css';
 import { useInjectFontsToDocumentHead } from '../hooks/use-inject-fonts-to-document-head';
+import { Scope } from 'react-shadow-scope';
+
+const stopPropagation = (e: Event) => e.stopPropagation();
 
 export const WithStyledShadowDom = ({ children }: { children: ReactNode }) => {
-  const [isClient, setIsClient] = useState(false);
+  useInjectFontsToDocumentHead();
+  const [shadowDom, setShadowDom] = useState<HTMLElement>();
+  const [styledComponentContainer, setStyledComponentContainer] =
+    useState<HTMLElement>();
+
+  const onShadowDomLoaded = useCallback((element: HTMLDivElement) => {
+    setShadowDom(element);
+    element?.addEventListener('wheel', stopPropagation, true);
+    element?.addEventListener('touchmove', stopPropagation, true);
+  }, []);
+
+  const onStyledComponentContainerLoaded = useCallback(
+    (element: HTMLDivElement) => {
+      setStyledComponentContainer(element);
+    },
+    []
+  );
 
   useEffect(() => {
-    setIsClient(true);
+    return () => {
+      shadowDom?.removeEventListener('wheel', stopPropagation, true);
+      shadowDom?.removeEventListener('touchmove', stopPropagation, true);
+    };
   }, []);
 
-  useInjectFontsToDocumentHead();
-
-  const onRefLoaded = useCallback((element: HTMLDivElement) => {
-    if (element !== null) {
-      const shadowRoot = element.attachShadow({ mode: 'open' });
-
-      const styleContainer = document.createElement('div');
-      const appContainer = document.createElement('div');
-      const hostStyle = document.createElement('style');
-      hostStyle.textContent = `
-        ${cssReset}
-        ${toastStyles}
-        ${shadowDomStyles}
-      `;
-
-      shadowRoot.appendChild(hostStyle);
-      shadowRoot.appendChild(styleContainer);
-      shadowRoot.appendChild(appContainer);
-
-      const Root = ({ children }: { children?: ReactNode }) => {
-        useEffect(() => {
-          const stopPropagation = (e: Event) => e.stopPropagation();
-
-          element?.shadowRoot?.addEventListener('wheel', stopPropagation, true);
-          element?.shadowRoot?.addEventListener(
-            'touchmove',
-            stopPropagation,
-            true
-          );
-          return () => {
-            element?.shadowRoot?.removeEventListener(
-              'wheel',
-              stopPropagation,
-              true
-            );
-            element?.shadowRoot?.removeEventListener(
-              'touchmove',
-              stopPropagation,
-              true
-            );
-          };
-        }, []);
-        return (
-          <StyleSheetManager target={styleContainer}>
-            {children}
-          </StyleSheetManager>
-        );
-      };
-
-      createRoot(appContainer).render(<Root>{children}</Root>);
-    }
-  }, []);
-
-  return isClient ? <div ref={onRefLoaded}></div> : null;
+  return (
+    <Scope
+      stylesheets={[cssReset, shadowDomStyles, toastStyles]}
+      config={{ dsd: 'emulated' }}
+      ref={onShadowDomLoaded}
+    >
+      <div ref={onStyledComponentContainerLoaded}></div>
+      <StyleSheetManager target={styledComponentContainer}>
+        {children}
+      </StyleSheetManager>
+    </Scope>
+  );
 };
