00130a0846184a6c65ab6d3bf1bc52596adc366c
todd kao <todd.kakao@gmail.com>
2024-08-16 14:04:52 -0400

    remove unused function

--- packages/widget-v2/src/utils/crypto.ts
+++ packages/widget-v2/src/utils/crypto.ts
@@ -8,13 +8,3 @@ export const getFormattedAssetAmount = (
   const result = amount / Math.pow(10, decimals ?? 6);
   return result.toFixed(decimals ?? 6).replace(/(\.\d*?[1-9])0+|\.0*$/, '$1');
 };
-
-export const formatCryptoAmount = (
-  amount: number | string,
-  decimals?: number
-) => {
-  if (typeof amount === 'string') {
-    amount = parseFloat(amount);
-  }
-  return amount.toFixed(decimals ?? 6).replace(/(\.\d*?[1-9])0+|\.0*$/, '$1');
-};
