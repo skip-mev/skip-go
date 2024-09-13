109b6518b272eae13694f16d60a97f2a8daaa3d4
Todd Kao <toddkao@users.noreply.github.com>
2024-09-13 17:30:52 -0400

    Add SwapBridgeArrow

--- packages/widget-v2/src/pages/SwapPage/SwapPageBridge.tsx
+++ packages/widget-v2/src/pages/SwapPage/SwapPageBridge.tsx
@@ -3,25 +3,40 @@ import { BridgeArrowIcon } from "@/icons/BridgeArrowIcon";
 import { BridgeIcon } from "@/icons/BridgeIcon";
 import { invertSwapAtom } from "@/state/swapPage";
 import { useSetAtom } from "jotai";
+import { useState } from "react";
 
 export const SwapPageBridge = () => {
   const theme = useTheme();
+  const [spin, setSpin] = useState(false);
   const invertSwap = useSetAtom(invertSwapAtom);
   const onInvertSwap = () => {
     invertSwap("swap-out");
+
+    let spinTimeout = undefined;
+    clearTimeout(spinTimeout);
+    setSpin(true);
+    spinTimeout = setTimeout(() => setSpin(false), 500);
   };
 
   return (
     <div style={{ position: "relative", cursor: "pointer" }} onClick={onInvertSwap}>
       <BridgeIcon color={theme.primary.background.normal} />
-      <StyledBridgeArrow color={theme.primary.text.normal} />
+      <StyledBridgeArrow spin={spin} color={theme.primary.text.normal} />
     </div>
   );
 };
 
-const StyledBridgeArrow = styled(BridgeArrowIcon)`
+const StyledBridgeArrow = styled(BridgeArrowIcon) <{ spin?: boolean }>`
   position: absolute;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
+
+  @keyframes spin {
+    50% {
+      transform: translate(-50%, -50%) rotate(180deg);
+    }
+  }
+
+  ${({ spin }) => spin && "animation: spin 0.5s cubic-bezier(.18,.89,.32,1.27);"};
 `;
