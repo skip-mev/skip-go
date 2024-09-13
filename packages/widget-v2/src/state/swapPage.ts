9030799f723c83d1a538bd277b2aafbdf500ce10
Todd Kao <toddkao@users.noreply.github.com>
2024-09-13 15:28:18 -0400

    Fix debouncedRouteRequestEffect (#239)

--- packages/widget-v2/src/state/swapPage.ts
+++ packages/widget-v2/src/state/swapPage.ts
@@ -3,14 +3,45 @@ import { ClientAsset, skipRouteAtom } from "./skipClient";
 import { Wallet } from "@/components/RenderWalletList";
 import { atomEffect } from "jotai-effect";
 import { parseAmountWei } from "@/utils/number";
+import { atomWithDebounce } from "@/utils/atomWithDebounce";
 
 export type AssetAtom = Partial<ClientAsset> & {
   amount?: string;
 };
 
+const ROUTE_REQUEST_DEBOUNCE_DELAY = 500;
+
+export const { debouncedValueAtom: debouncedSourceAssetAmount } = atomWithDebounce<string | undefined>(
+  undefined,
+  ROUTE_REQUEST_DEBOUNCE_DELAY,
+);
+
+export const { debouncedValueAtom: debouncedDestinationAssetAmount } = atomWithDebounce<string | undefined>(
+  undefined,
+  ROUTE_REQUEST_DEBOUNCE_DELAY,
+);
+
 export const sourceAssetAtom = atom<AssetAtom>();
 
+export const sourceAssetAmount = atom(
+  (get) => get(sourceAssetAtom)?.amount,
+  (get, set, newAmount: string) => {
+    const oldSourceAsset = get(sourceAssetAtom);
+    set(sourceAssetAtom, { ...oldSourceAsset, amount: newAmount });
+    set(debouncedSourceAssetAmount, newAmount);
+  },
+);
+
 export const destinationAssetAtom = atom<AssetAtom>();
+
+export const destinationAssetAmount = atom(
+  (get) => get(destinationAssetAtom)?.amount,
+  (get, set, newAmount: string) => {
+    const oldDestinationAsset = get(destinationAssetAtom);
+    set(destinationAssetAtom, { ...oldDestinationAsset, amount: newAmount });
+    set(debouncedDestinationAssetAmount, newAmount);
+  },
+);
 
 export const swapDirectionAtom = atom<"swap-in" | "swap-out">("swap-in");
 
@@ -18,29 +49,28 @@ export const connectedWalletAtom = atom<Wallet>();
 
 export const destinationWalletAtom = atom<Wallet>();
 
-
 export const routeAmountEffect = atomEffect((get, set) => {
-  const route = get(skipRouteAtom)
-  const direction = get(swapDirectionAtom)
-  const sourceAsset = get(sourceAssetAtom)
-  const destinationAsset = get(destinationAssetAtom)
+  const route = get(skipRouteAtom);
+  const direction = get(swapDirectionAtom);
+  const sourceAsset = get(sourceAssetAtom);
+  const destinationAsset = get(destinationAssetAtom);
 
-  const isSwapIn = direction === "swap-in";
+  if (!route.data || !sourceAsset || !destinationAsset) return;
 
-  if (!route.data || !sourceAsset || !destinationAsset) return
+  const swapInAmount = parseAmountWei(route.data.amountOut, destinationAsset.decimals);
+  const swapOutAmount = parseAmountWei(route.data.amountIn, sourceAsset.decimals);
+  const swapInAmountChanged = swapInAmount !== destinationAsset.amount;
+  const swapOutAmountChanged = swapOutAmount !== sourceAsset.amount;
 
-  if (isSwapIn) {
-    const amount = parseAmountWei(route.data.amountOut, destinationAsset.decimals)
+  if (direction === "swap-in" && swapInAmountChanged) {
     set(destinationAssetAtom, (old) => ({
       ...old,
-      amount,
-    }))
-  }
-  else {
-    const amount = parseAmountWei(route.data.amountIn, sourceAsset.decimals)
+      amount: swapInAmount,
+    }));
+  } else if (direction === "swap-out" && swapOutAmountChanged) {
     set(sourceAssetAtom, (old) => ({
       ...old,
-      amount,
-    }))
+      amount: swapOutAmount,
+    }));
   }
-})
+});
