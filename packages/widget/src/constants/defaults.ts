0c1cfde63d2eaa00b21bbdd077ed2ea80d0d1308
Eric Gorski <er.h.gorski@gmail.com>
2024-09-20 15:18:16 -0400

    revert

--- packages/widget/src/constants/defaults.ts
+++ packages/widget/src/constants/defaults.ts
@@ -2,10 +2,7 @@ const appUrl = 'https://go.skip.build';
 
 export const endpointOptions = {
   getRpcEndpointForChain: async (chainID: string) => {
-    if (chainID === 'solana') {
-      return 'https://api.mainnet-beta.solana.com';
-    }
-    else return `${appUrl}/api/rpc/${chainID}`;
+    return `${appUrl}/api/rpc/${chainID}`;
   },
   getRestEndpointForChain: async (chainID: string) => {
     return `${appUrl}/api/rest/${chainID}`;
