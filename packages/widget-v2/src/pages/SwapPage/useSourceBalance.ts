a6307f2fbce178c7ebad8cb9544ca0bd43e8fe7c
Nur Fikri <codingki@gmail.com>
2024-09-26 23:39:17 +0700

    feat(widget-v2)/balance loading state

--- packages/widget-v2/src/pages/SwapPage/useSourceBalance.ts
+++ packages/widget-v2/src/pages/SwapPage/useSourceBalance.ts
@@ -2,15 +2,24 @@ import { useAccount } from "@/hooks/useAccount";
 import { skipBalancesAtom } from "@/state/skipClient";
 import { sourceAssetAtom } from "@/state/swapPage";
 import { useAtom, useAtomValue } from "jotai";
+import { useMemo } from "react";
 
 export const useSourceBalance = () => {
   const [sourceAsset] = useAtom(sourceAssetAtom);
   const sourceAccount = useAccount(sourceAsset?.chainID);
-  const { data: skipBalances } = useAtomValue(skipBalancesAtom);
+  const { data: skipBalances, isLoading, refetch } = useAtomValue(skipBalancesAtom);
 
-  if (!sourceAsset || !sourceAccount || !skipBalances) return;
-  const { chainID, denom } = sourceAsset;
-  if (!denom || !chainID) return;
+  const data = useMemo(() => {
+    if (!sourceAsset || !sourceAccount || !skipBalances) return;
+    const { chainID, denom } = sourceAsset;
+    if (!denom || !chainID) return;
 
-  return skipBalances?.chains?.[chainID]?.denoms?.[denom];
+    return skipBalances?.chains?.[chainID]?.denoms?.[denom];
+  }, [sourceAsset, sourceAccount, skipBalances]);
+
+  return {
+    data,
+    isLoading,
+    refetch
+  };
 };
