d69f96e92dd717420e4b5ea7252e297aa4551916
todd kao <todd.kakao@gmail.com>
2024-09-28 10:26:32 -0400

    test removing formats

--- packages/widget-v2/vite.config.ts
+++ packages/widget-v2/vite.config.ts
@@ -33,7 +33,6 @@ export default defineConfig({
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, "src/index.tsx"),
-      formats: ["es"],
       name: "widget-v2",
     },
     sourcemap: true,
