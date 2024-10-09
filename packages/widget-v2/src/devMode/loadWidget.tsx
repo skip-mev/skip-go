3e1e87fa4ae4ff291660a421ed521dee29df4e50
todd kao <todd.kakao@gmail.com>
2024-10-09 11:58:24 -0400

    Add light mode next to default theme

--- packages/widget-v2/src/devMode/loadWidget.tsx
+++ packages/widget-v2/src/devMode/loadWidget.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { SwapWidget } from "@/widget/Widget";
 import { Row } from "@/components/Layout";
+import { lightTheme } from "@/widget/theme";
 
 createRoot(document.getElementById("root") as HTMLElement)?.render(
   <StrictMode>
     <Row gap={20}>
       <SwapWidget />
+      <SwapWidget theme={lightTheme} />
     </Row>
   </StrictMode>,
 );
