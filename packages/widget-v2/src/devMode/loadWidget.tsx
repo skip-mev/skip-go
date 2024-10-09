69f34e0064c4fa316e86ff988e15c7222df593fc
todd kao <todd.kakao@gmail.com>
2024-10-09 12:41:37 -0400

    Add toggle theme button

--- packages/widget-v2/src/devMode/loadWidget.tsx
+++ packages/widget-v2/src/devMode/loadWidget.tsx
@@ -1,14 +1,31 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 import { SwapWidget } from "@/widget/Widget";
-import { Row } from "@/components/Layout";
-import { lightTheme } from "@/widget/theme";
+import { Row, Column } from "@/components/Layout";
+import { defaultTheme, lightTheme } from "@/widget/theme";
+
+const DevMode = () => {
+  const [theme, setTheme] = useState<"dark" | "light">("dark");
+
+  const toggleTheme = () => {
+    if (theme === "dark") {
+      setTheme("light");
+    } else {
+      setTheme("dark");
+    }
+  };
+  return (
+    <Row gap={20}>
+      <SwapWidget theme={theme === "dark" ? defaultTheme : lightTheme} />
+      <Column>
+        <button onClick={() => toggleTheme()}> Toggle theme (current theme: {theme})</button>
+      </Column>
+    </Row>
+  );
+};
 
 createRoot(document.getElementById("root") as HTMLElement)?.render(
   <StrictMode>
-    <Row gap={20}>
-      <SwapWidget />
-      <SwapWidget theme={lightTheme} />
-    </Row>
-  </StrictMode>,
+    <DevMode />
+  </StrictMode>
 );
