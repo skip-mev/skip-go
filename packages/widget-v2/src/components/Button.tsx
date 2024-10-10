89ddffa1dc2ccab95db135aad5e402efbb8515f8
todd kao <todd.kakao@gmail.com>
2024-10-10 17:54:24 -0400

    Revert "Add not-allowed cursor for disabled GhostButton"
    This reverts commit 54d3dba9e9bfeac9d86c454e8241b46a29f602e2.

--- packages/widget-v2/src/components/Button.tsx
+++ packages/widget-v2/src/components/Button.tsx
@@ -37,19 +37,6 @@ export const GhostButton = styled(SmallText).attrs({
         `;
     }
   }}
-
-  ${({ disabled }) =>
-    disabled
-      ? css`
-        &:hover {
-          cursor: not-allowed;
-        }
-      `
-      : css`
-        &:hover {
-          cursor: pointer;
-        }
-      `}
  
   padding: 8px 16px;
   border-radius: 90px;
