1f82f3eea4dbb3468178d2a6fadcccaa62bc320a
Todd Kao <toddkao@users.noreply.github.com>
2024-10-10 15:47:57 -0400

    Add background to nextjs example (#353)

--- examples/nextjs/pages/index.tsx
+++ examples/nextjs/pages/index.tsx
@@ -1,15 +1,18 @@
 import { SwapWidget } from '@skip-go/widget';
 import { defaultTheme, lightTheme } from '@skip-go/widget';
+import darkbg from './gobg-dark.svg';
+import lightbg from './gobg-light.svg';
+
 import { useState } from 'react';
 
 const Home = () => {
-  const [theme, setTheme] = useState<"dark" | "light">("dark");
+  const [theme, setTheme] = useState<'dark' | 'light'>('dark');
 
   const toggleTheme = () => {
-    if (theme === "dark") {
-      setTheme("light");
+    if (theme === 'dark') {
+      setTheme('light');
     } else {
-      setTheme("dark");
+      setTheme('dark');
     }
   };
 
@@ -18,12 +21,30 @@ const Home = () => {
       style={{
         display: 'flex',
         flexDirection: 'row',
-        padding: 20
+        width: '100vw',
+        height: '100vh',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundImage: `url('${theme === 'dark' ? darkbg.src : lightbg.src
+          }')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
       }}
     >
-      <SwapWidget theme={theme === "dark" ? defaultTheme : lightTheme} />
-      <div>
-        <button onClick={() => toggleTheme()}> Toggle theme (current theme: {theme})</button>
+      <button
+        style={{ position: 'absolute', top: 0, right: 0 }}
+        onClick={() => toggleTheme()}
+      >
+        Toggle theme (current theme: {theme})
+      </button>
+      <div
+        style={{
+          position: 'absolute',
+          top: '50%',
+          transform: 'translateY(-185px)',
+        }}
+      >
+        <SwapWidget theme={theme === 'dark' ? defaultTheme : lightTheme} />
       </div>
     </div>
   );
