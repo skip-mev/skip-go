7f1dae388be8c12a2ebbf9158771335041e9d472
Nur Fikri <codingki@gmail.com>
2024-10-24 06:43:34 +0700

    add debounce for balance request

--- packages/widget-v2/src/hooks/useFetchAllBalances.ts
+++ packages/widget-v2/src/hooks/useFetchAllBalances.ts
@@ -1,51 +1,53 @@
 import { sourceAssetAtom } from "@/state/swapPage";
 import { useAtomValue, useSetAtom } from "jotai";
 import { useGetAccount } from "./useGetAccount";
-import { skipAllBalancesRequestAtom, skipAllBalancesAtom } from "@/state/balances";
-import { useEffect } from "react";
+import { skipAllBalancesRequestAtom } from "@/state/balances";
 import { skipAssetsAtom, skipChainsAtom } from "@/state/skipClient";
 import { useAccount } from "wagmi";
+import { useQuery } from "@tanstack/react-query";
 
 export const useFetchAllBalances = () => {
   const sourceAsset = useAtomValue(sourceAssetAtom);
   const getAccount = useGetAccount();
   const { data: assets } = useAtomValue(skipAssetsAtom);
-  const setSkipAllBalancesRequest = useSetAtom(skipAllBalancesRequestAtom);
-  const { data: skipBalances } = useAtomValue(skipAllBalancesAtom);
+  const setSkipAllBalancesRequest = useSetAtom(skipAllBalancesRequestAtom.debouncedValueAtom);
   const { data: chains } = useAtomValue(skipChainsAtom);
 
   const { chainId: evmChainId } = useAccount();
 
-  useEffect(() => {
-    const allBalancesRequest = assets?.reduce((acc, asset) => {
-      const address = getAccount(asset.chainID)?.address;
-      const chain = chains?.find((chain) => chain.chainID === asset.chainID);
-      const isEVM = chain?.chainType === "evm";
-      const evmAddress = (isEVM && evmChainId) ? getAccount(String(evmChainId))?.address : undefined;
-      if (isEVM && evmAddress) {
-        if (asset.denom === sourceAsset?.denom) return acc;
-        if (!acc[asset.chainID]) {
-          acc[asset.chainID] = {
-            address: evmAddress,
-          };
-        }
-      } else if (address) {
-        if (asset.denom === sourceAsset?.denom) return acc;
-        if (!acc[asset.chainID]) {
-          acc[asset.chainID] = {
-            address: address,
-          };
-        }
+  const allBalancesRequest = assets?.reduce((acc, asset) => {
+    const address = getAccount(asset.chainID)?.address;
+    const chain = chains?.find((chain) => chain.chainID === asset.chainID);
+    const isEVM = chain?.chainType === "evm";
+    const evmAddress = (isEVM && evmChainId) ? getAccount(String(evmChainId))?.address : undefined;
+    if (isEVM && evmAddress) {
+      if (asset.denom === sourceAsset?.denom) return acc;
+      if (!acc[asset.chainID]) {
+        acc[asset.chainID] = {
+          address: evmAddress,
+        };
       }
-      return acc;
-    }, {} as Record<string, { address: string }>);
-
-    if (allBalancesRequest) {
-      setSkipAllBalancesRequest({
-        chains: allBalancesRequest || {}
-      });
+    } else if (address) {
+      if (asset.denom === sourceAsset?.denom) return acc;
+      if (!acc[asset.chainID]) {
+        acc[asset.chainID] = {
+          address: address,
+        };
+      }
     }
-  }, [assets, evmChainId, chains, getAccount, setSkipAllBalancesRequest, sourceAsset]);
+    return acc;
+  }, {} as Record<string, { address: string }>);
 
-  return skipBalances;
+  // using useQuery to trigger the debouncedValueAtom
+  useQuery({
+    queryKey: ["test", allBalancesRequest],
+    queryFn: () => {
+      if (allBalancesRequest) {
+        setSkipAllBalancesRequest({
+          chains: allBalancesRequest || {}
+        });
+      }
+      return null;
+    }
+  });
 };
