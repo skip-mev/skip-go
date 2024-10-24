b9f70d8be475d29cf4bd8fcb38bc2ac191dcf837
Nur Fikri <codingki@gmail.com>
2024-10-24 08:16:37 +0700

    fix no source asset balance

--- packages/widget-v2/src/hooks/useFetchAllBalances.ts
+++ packages/widget-v2/src/hooks/useFetchAllBalances.ts
@@ -1,13 +1,12 @@
-import { sourceAssetAtom } from "@/state/swapPage";
 import { useAtomValue, useSetAtom } from "jotai";
 import { useGetAccount } from "./useGetAccount";
 import { skipAllBalancesRequestAtom } from "@/state/balances";
 import { skipAssetsAtom, skipChainsAtom } from "@/state/skipClient";
 import { useAccount } from "wagmi";
 import { useQuery } from "@tanstack/react-query";
+import { useMemo } from "react";
 
 export const useFetchAllBalances = () => {
-  const sourceAsset = useAtomValue(sourceAssetAtom);
   const getAccount = useGetAccount();
   const { data: assets } = useAtomValue(skipAssetsAtom);
   const setSkipAllBalancesRequest = useSetAtom(skipAllBalancesRequestAtom.debouncedValueAtom);
@@ -15,28 +14,28 @@ export const useFetchAllBalances = () => {
 
   const { chainId: evmChainId } = useAccount();
 
-  const allBalancesRequest = assets?.reduce((acc, asset) => {
-    const address = getAccount(asset.chainID)?.address;
-    const chain = chains?.find((chain) => chain.chainID === asset.chainID);
-    const isEVM = chain?.chainType === "evm";
-    const evmAddress = (isEVM && evmChainId) ? getAccount(String(evmChainId))?.address : undefined;
-    if (isEVM && evmAddress) {
-      if (asset.denom === sourceAsset?.denom) return acc;
-      if (!acc[asset.chainID]) {
-        acc[asset.chainID] = {
-          address: evmAddress,
-        };
+  const allBalancesRequest = useMemo(() => {
+    return assets?.reduce((acc, asset) => {
+      const address = getAccount(asset.chainID)?.address;
+      const chain = chains?.find((chain) => chain.chainID === asset.chainID);
+      const isEVM = chain?.chainType === "evm";
+      const evmAddress = (isEVM && evmChainId) ? getAccount(String(evmChainId))?.address : undefined;
+      if (isEVM && evmAddress) {
+        if (!acc[asset.chainID]) {
+          acc[asset.chainID] = {
+            address: evmAddress,
+          };
+        }
+      } else if (address) {
+        if (!acc[asset.chainID]) {
+          acc[asset.chainID] = {
+            address: address,
+          };
+        }
       }
-    } else if (address) {
-      if (asset.denom === sourceAsset?.denom) return acc;
-      if (!acc[asset.chainID]) {
-        acc[asset.chainID] = {
-          address: address,
-        };
-      }
-    }
-    return acc;
-  }, {} as Record<string, { address: string }>);
+      return acc;
+    }, {} as Record<string, { address: string }>);
+  }, [assets, getAccount, chains, evmChainId]);
 
   // using useQuery to trigger the debouncedValueAtom
   useQuery({
