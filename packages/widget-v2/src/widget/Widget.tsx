0b99a4e1faa50db284b616f45c5e15c3e4fddf25
Todd Kao <todd.kakao@gmail.com>
2024-10-25 19:40:24 -0400

    Revert re-render pr

--- packages/widget-v2/src/widget/Widget.tsx
+++ packages/widget-v2/src/widget/Widget.tsx
@@ -2,29 +2,18 @@ import { ShadowDomAndProviders } from "./ShadowDomAndProviders";
 import NiceModal from "@ebay/nice-modal-react";
 import { styled } from "styled-components";
 import { createModal, useModal } from "@/components/Modal";
-import { cloneElement, ReactElement, useCallback, useEffect, useMemo } from "react";
-import { defaultTheme, lightTheme, PartialTheme, Theme } from "./theme";
+import { cloneElement, ReactElement, useEffect } from "react";
+import { defaultTheme, PartialTheme } from "./theme";
 import { Router } from "./Router";
 import { useResetAtom } from "jotai/utils";
 import { numberOfModalsOpenAtom } from "@/state/modal";
 import { useAtom, useSetAtom } from "jotai";
-import { defaultSkipClientConfig, skipAssetsAtom, skipClientConfigAtom, themeAtom } from "@/state/skipClient";
+import { skipClientConfigAtom, themeAtom } from "@/state/skipClient";
 import { SkipClientOptions } from "@skip-go/client";
-import { destinationAssetAmountAtom, destinationAssetAtom, sourceAssetAmountAtom, sourceAssetAtom } from "@/state/swapPage";
-
-export type DefaultRouteConfig = {
-  amountIn?: number;
-  amountOut?: number;
-  srcChainID?: string;
-  srcAssetDenom?: string;
-  destChainID?: string;
-  destAssetDenom?: string;
-}
 
 export type WidgetProps = {
-  theme?: PartialTheme | "light" | "dark";
+  theme?: PartialTheme;
   brandColor?: string;
-  defaultRoute?: DefaultRouteConfig;
 } & SkipClientOptions;
 
 export const Widget = (props: WidgetProps) => {
@@ -35,82 +24,19 @@ export const Widget = (props: WidgetProps) => {
   );
 };
 
-const WidgetWithoutNiceModalProvider = (props: WidgetProps) => {
-  const setSkipClientConfig = useSetAtom(skipClientConfigAtom);
+const WidgetWithoutNiceModalProvider = ({ theme, ...skipClientConfig }: WidgetProps) => {
+  const [defaultSkipClientConfig, setSkipClientConfig] = useAtom(skipClientConfigAtom);
   const setTheme = useSetAtom(themeAtom);
-  // const setSourceAsset = useSetAtom(sourceAssetAtom);
-  // const setDestinationAsset = useSetAtom(destinationAssetAtom);
-  // const setSourceAssetAmount = useSetAtom(sourceAssetAmountAtom);
-  // const setDestinationAssetAmount = useSetAtom(destinationAssetAmountAtom);
-
-  // const [{ data: assets }] = useAtom(skipAssetsAtom);
-
-  // const getClientAsset = useCallback(
-  //   (denom?: string, chainId?: string) => {
-  //     if (!denom || !chainId) return;
-  //     if (!assets) return;
-  //     return assets.find((a) => a.denom === denom && a.chainID === chainId);
-  //   },
-  //   [assets]
-  // );
-
-  const mergedSkipClientConfig = useMemo(
-    () => {
-      const { theme, ...skipClientConfig } = props;
-
-      return {
-        ...defaultSkipClientConfig,
-        ...skipClientConfig,
-      };
-    },
-    [props]
-  );
-
-  const mergedTheme = useMemo(() => {
-    let theme: Theme;
-    if (typeof props.theme === "string") {
-      theme = props.theme === "light" ? lightTheme : defaultTheme;
-    } else {
-      theme = { ...defaultTheme, ...props.theme };
-    }
-    if (props.brandColor) {
-      theme.brandColor = props.brandColor;
-    }
-    return theme;
-  }, [props.brandColor, props.theme]);
-
   useEffect(() => {
-    setSkipClientConfig(mergedSkipClientConfig);
-    setTheme(mergedTheme);
-    // if (props.defaultRoute && assets) {
-    //   const {
-    //     srcAssetDenom,
-    //     srcChainID,
-    //     destAssetDenom,
-    //     destChainID,
-    //     amountIn,
-    //     amountOut,
-    //   } = props.defaultRoute;
-    //   const sourceAsset = getClientAsset(srcAssetDenom, srcChainID);
-    //   const destinationAsset = getClientAsset(destAssetDenom, destChainID);
-    //   setDestinationAsset({
-    //     ...destinationAsset,
-    //     amount: amountOut?.toString(),
-    //   });
-    //   setSourceAsset({
-    //     ...sourceAsset,
-    //     amount: amountIn?.toString(),
-    //   });
-    //   if (amountIn) {
-    //     setSourceAssetAmount(amountIn?.toString());
-    //   } else if (amountOut) {
-    //     setDestinationAssetAmount(amountOut?.toString());
-    //   }
-    // }
-  }, [mergedSkipClientConfig, mergedTheme, setSkipClientConfig, setTheme]);
+    setSkipClientConfig({
+      ...defaultSkipClientConfig,
+      ...skipClientConfig,
+    });
+    setTheme({ ...defaultTheme, ...theme });
+  }, [defaultSkipClientConfig, setSkipClientConfig, setTheme, skipClientConfig, theme]);
 
   return (
-    <ShadowDomAndProviders theme={mergedTheme}>
+    <ShadowDomAndProviders theme={theme}>
       <WidgetContainer>
         <Router />
       </WidgetContainer>
