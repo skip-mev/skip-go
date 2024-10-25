992767920db8f2c251760bd59733bcf7d061c622
Nur Fikri <codingki@gmail.com>
2024-10-26 00:15:57 +0700

    add debounce for balance request (#375)
    Co-authored-by: plubber <51789398+ericHgorski@users.noreply.github.com>

--- packages/widget-v2/src/state/balances.ts
+++ packages/widget-v2/src/state/balances.ts
@@ -1,14 +1,81 @@
 import { BalanceRequest } from "@skip-go/client";
-import { atom } from "jotai";
 import { atomWithQuery } from "jotai-tanstack-query";
 import { skipClient } from "./skipClient";
 import { isInvertingSwapAtom } from "./swapPage";
 
-export const skipAllBalancesRequestAtom = atom<BalanceRequest | undefined>();
+import { atom, SetStateAction } from "jotai";
+
+// making this internal because useWithDebounce is conflicting with the original atomWithDebounce in utils
+function atomWithDebounce<T>(
+  initialValue: T,
+  delayMilliseconds = 500,
+  shouldDebounceOnReset = false,
+) {
+  const prevTimeoutAtom = atom<ReturnType<typeof setTimeout> | undefined>(
+    undefined,
+  );
+
+  // DO NOT EXPORT currentValueAtom as using this atom to set state can cause
+  // inconsistent state between currentValueAtom and debouncedValueAtom
+  const _currentValueAtom = atom(initialValue);
+  const isDebouncingAtom = atom(false);
+
+  const debouncedValueAtom = atom(
+    initialValue,
+    (get, set, update: SetStateAction<T>) => {
+      clearTimeout(get(prevTimeoutAtom));
+
+      const prevValue = get(_currentValueAtom);
+      const nextValue =
+        typeof update === "function"
+          ? (update as (prev: T) => T)(prevValue)
+          : update;
+
+      const onDebounceStart = () => {
+        set(_currentValueAtom, nextValue);
+        set(isDebouncingAtom, true);
+      };
+
+      const onDebounceEnd = () => {
+        set(debouncedValueAtom, nextValue);
+        set(isDebouncingAtom, false);
+      };
+
+      onDebounceStart();
+
+      if (!shouldDebounceOnReset && nextValue === initialValue) {
+        onDebounceEnd();
+        return;
+      }
+
+      const nextTimeoutId = setTimeout(() => {
+        onDebounceEnd();
+      }, delayMilliseconds);
+
+      // set previous timeout atom in case it needs to get cleared
+      set(prevTimeoutAtom, nextTimeoutId);
+    },
+  );
+
+  // exported atom setter to clear timeout if needed
+  const clearTimeoutAtom = atom(null, (get, set, _arg) => {
+    clearTimeout(get(prevTimeoutAtom));
+    set(isDebouncingAtom, false);
+  });
+
+  return {
+    currentValueAtom: atom((get) => get(_currentValueAtom)),
+    isDebouncingAtom,
+    clearTimeoutAtom,
+    debouncedValueAtom,
+  };
+}
+
+export const skipAllBalancesRequestAtom = atomWithDebounce<BalanceRequest | undefined>(undefined);
 
 export const skipAllBalancesAtom = atomWithQuery((get) => {
   const skip = get(skipClient);
-  const params = get(skipAllBalancesRequestAtom);
+  const params = get(skipAllBalancesRequestAtom.debouncedValueAtom);
   const isInvertingSwap = get(isInvertingSwapAtom);
 
   const enabled = Object.values(params ?? {}).length > 0 && !isInvertingSwap;
