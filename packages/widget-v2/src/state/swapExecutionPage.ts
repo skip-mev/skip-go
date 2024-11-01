7653d2ae820dcc254a4ddd3722b63070fcd91d99
plubber <51789398+ericHgorski@users.noreply.github.com>
2024-11-01 13:42:56 -0400

    [FRE-1192] add slippage to route execution (#409)

--- packages/widget-v2/src/state/swapExecutionPage.ts
+++ packages/widget-v2/src/state/swapExecutionPage.ts
@@ -220,6 +220,7 @@ export const skipSubmitSwapExecutionAtom = atomWithMutation((get) => {
         await skip.executeRoute({
           route,
           userAddresses,
+          slippageTolerancePercent: swapSettings.slippage.toString(),
           validateGasBalance: route.sourceAssetChainID !== "984122",
           getFallbackGasAmount: async (_chainID, chainType) => {
             if (chainType === "cosmos") {
