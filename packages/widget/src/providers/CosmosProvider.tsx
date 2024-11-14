786f396e9525eba6fd646e9ee0b77e855baa8946
Nur Fikri <codingki@gmail.com>
2024-11-15 04:51:10 +0700

    override projectId

--- packages/widget/src/providers/CosmosProvider.tsx
+++ packages/widget/src/providers/CosmosProvider.tsx
@@ -25,8 +25,7 @@ export const CosmosProvider: React.FC<CosmosProviderProps> = ({
       },
       walletConnect: {
         options: {
-          projectId: walletConnect?.options?.projectId,
-          name: walletConnect?.options?.name,
+          projectId: "baea98874b230c2a8d9c0ae32a98677a",
         },
         web3Modal: walletConnect?.web3Modal
       }
