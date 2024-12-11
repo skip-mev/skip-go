bdfa6b9d0de1077f4f6cf0a91fcf8ab587f26003
Nur Fikri <codingki@gmail.com>
2024-12-12 01:52:51 +0700

    fix useShowCosmosLedgerWarning (#564)

--- packages/widget/src/hooks/useShowCosmosLedgerWarning.ts
+++ packages/widget/src/hooks/useShowCosmosLedgerWarning.ts
@@ -1,30 +1,28 @@
 import { skipChainsAtom } from "@/state/skipClient";
 import { sourceAssetAtom } from "@/state/swapPage";
 import { knownEthermintLikeChains } from "@/state/wallets"
-import { useAccount } from "graz";
 import { useAtomValue } from "jotai"
 import { useMemo } from "react";
 import { ChainType } from "@skip-go/client";
+import { useGetAccount } from "./useGetAccount";
 
 export const useShowCosmosLedgerWarning = () => {
   const { data: chains } = useAtomValue(skipChainsAtom);
   const sourceAsset = useAtomValue(sourceAssetAtom);
   const chainType = chains?.find((c) => c.chainID === sourceAsset?.chainID)?.chainType;
-  if (chainType !== ChainType.Cosmos) return false;
-  const { data: cosmosAccount } = useAccount({
-    chainId: sourceAsset?.chainID,
-  })
+  const getAccount = useGetAccount();
 
   return useMemo(() => {
-    if (!cosmosAccount?.bech32Address) return false;
-    if (!sourceAsset?.chainID) return false;
     if (chainType !== ChainType.Cosmos) return false;
+    const account = getAccount(sourceAsset?.chainID);
+    if (!account?.address) return false;
+    if (!sourceAsset?.chainID) return false;
     if (!knownEthermintLikeChains.includes(sourceAsset?.chainID)) return false;
-    return !!cosmosAccount?.isNanoLedger
+    return !!account?.wallet.isLedger
   }, [
     sourceAsset?.chainID,
     chainType,
-    cosmosAccount?.isNanoLedger,
-    cosmosAccount?.bech32Address,
+    getAccount,
+    knownEthermintLikeChains,
   ])
 }
