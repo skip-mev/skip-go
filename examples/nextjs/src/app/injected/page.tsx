dc2450fad1b123ac619502dd4df085e1ed2b37d1
plubber <51789398+ericHgorski@users.noreply.github.com>
2024-12-12 17:57:11 -0500

    wip

--- examples/nextjs/src/app/injected/page.tsx
+++ examples/nextjs/src/app/injected/page.tsx
@@ -1,34 +1,25 @@
 'use client';
 import { Widget } from '@skip-go/widget';
 import { useState } from 'react';
-import { useQueryParams } from '@/hooks/useURLQueryParams';
+
+type ChainId = string;
+type Bech32Address = string;
 
 export default function Home() {
-  // optional theme, widget will be dark mode be default
-  const [theme, setTheme] = useState<'dark' | 'light'>('dark');
-  // optional query params, not necessary for the widget to work
-  const defaultRoute = useQueryParams();
+  const [account, setAccount] = useState<Record<ChainId, Bech32Address>>()
+  // @ts-ignore
+  const keplr = window.keplr as unknown as any;
 
-  const toggleTheme = () => {
-    if (theme === 'dark') {
-      setTheme('light');
-    } else {
-      setTheme('dark');
-    }
-  };
-  const [account, setAccount] = useState<Record<string, string>>()
+  if (!keplr) {
+    alert('Please install keplr extension');
+    return;
+  }
+  if (!keplr.experimentalSuggestChain) {
+    alert('Please update keplr extension');
+    return;
+  }
 
   const connectKeplr = async () => {
-    // @ts-ignore
-    const keplr = window.keplr as unknown as any;
-    if (!keplr) {
-      alert('Please install keplr extension');
-      return;
-    }
-    if (!keplr.experimentalSuggestChain) {
-      alert('Please update keplr extension');
-      return;
-    }
     const chainIds = ['cosmoshub-4', 'osmosis-1'];
 
     await keplr.enable(chainIds);
@@ -42,95 +33,46 @@ export default function Home() {
         }
       })
     }))
+  }
 
+  const getCosmosSigner = async (chainId: string) => {
+    try {
+      const offlineSigner = await keplr.getOfflineSigner(chainId);
+      return offlineSigner
+    } catch (error) {
+      alert(`${chainId} is not connected in Keplr`);
+    }
   }
 
   return (
-    <div
-      style={{
+    <div style={{
+      width: '100%',
+      maxWidth: 500,
+      padding: '0 10px',
+      boxSizing: 'border-box'
+    }}>
+      <p>Connected address:</p>
+      <ul>
+        {Object.entries(account ?? {}).map(([chainId, bech32Address]) => {
+          return <li key={chainId}>{chainId}: {bech32Address}</li>
+        })}
+      </ul>
+      <div style={{
         display: 'flex',
-        flexDirection: 'row',
-        width: '100vw',
-        height: '100vh',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundImage: `url('${theme === 'dark' ? '/gobg-dark.svg' : '/gobg-light.svg'
-          }')`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'bottom',
-      }}
-    >
-      <button
-        style={{ position: 'absolute', top: 0, right: 0 }}
-        onClick={() => toggleTheme()}
-      >
-        Toggle theme (current theme: {theme})
-      </button>
-      <div
-        style={{
-          position: 'absolute',
-          top: '50%',
-          transform: 'translateY(-185px)',
-          width: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        {/* widget will cohere to the parent container's width */}
-        <div style={{
-          width: '100%',
-          maxWidth: 500,
-          padding: '0 10px',
-          boxSizing: 'border-box'
-        }}>
-          <p>Connected address:</p>
-          <ul>
-            {Object.entries(account ?? {}).map(([chainId, bech32Address]) => {
-              return <li key={chainId}>{chainId}: {bech32Address}</li>
-            })}
-          </ul>
-          <div style={{
-            display: 'flex',
-            flexDirection: 'column',
-            gap: '10px'
-          }}>
-            <button onClick={connectKeplr}>Connect cosmoshub and osmosis with Keplr</button>
-            <button onClick={
-              () => {
-                setAccount(undefined)
-              }
-            }>Disconnect</button>
-          </div>
-          <Widget
-            theme={theme}
-            defaultRoute={{
-              srcChainId: 'cosmoshub-4',
-              srcAssetDenom: 'uatom',
-              destChainId: 'osmosis-1',
-              destAssetDenom: 'uosmo',
-            }}
-            connectedAddress={account}
-            getCosmosSigner={async (chainId) => {
-              // @ts-ignore
-              const keplr = window.keplr as unknown as any;
-              if (!keplr) {
-                alert('Please install keplr extension');
-                return;
-              }
-              if (!keplr.getOfflineSigner) {
-                return;
-              }
-              try {
-                const offlineSigner = await keplr.getOfflineSigner(chainId);
-                return offlineSigner
-              } catch (error) {
-                alert(`${chainId} is not connected in Keplr`);
-              }
-            }}
-          />
-        </div>
+        flexDirection: 'column',
+        gap: '10px'
+      }}>
+        <button onClick={connectKeplr}>Connect cosmoshub and osmosis with Keplr</button>
+        <button onClick={
+          () => {
+            setAccount(undefined)
+          }
+        }>Disconnect</button>
       </div>
+      <Widget
+        connectedAddress={account}
+        getCosmosSigner={getCosmosSigner}
+      />
     </div>
   );
 }
