e5c31f2d15c3ab602994af4cf1ee9f99571eeac7
Todd Kao <toddkao@users.noreply.github.com>
2025-01-14 12:22:36 -0500

    [FRE-1392] Manually configure integrations to disable globalHandlers and BrowserApiErrors (#654)

--- packages/widget/src/index.tsx
+++ packages/widget/src/index.tsx
@@ -2,11 +2,30 @@ export { Widget, ShowWidget } from "./widget/Widget";
 export type { WidgetProps } from "./widget/Widget";
 export { defaultTheme, lightTheme } from "./widget/theme";
 
-import { init, replayIntegration } from "@sentry/react";
+import {
+  breadcrumbsIntegration,
+  browserSessionIntegration,
+  dedupeIntegration,
+  functionToStringIntegration,
+  httpContextIntegration,
+  inboundFiltersIntegration,
+  init,
+  linkedErrorsIntegration,
+  replayIntegration,
+} from "@sentry/react";
 
 init({
   dsn: "https://10ce608bdd1c68a13d3849d6b242333c@o4504768725909504.ingest.us.sentry.io/4508485201231872",
+  defaultIntegrations: false,
+  denyUrls: [/^https?:\/\/localhost:.*/],
   integrations: [
+    breadcrumbsIntegration(),
+    dedupeIntegration(),
+    functionToStringIntegration(),
+    httpContextIntegration(),
+    inboundFiltersIntegration(),
+    linkedErrorsIntegration(),
+    browserSessionIntegration(),
     replayIntegration({
       maskAllText: false,
       maskAllInputs: false,
