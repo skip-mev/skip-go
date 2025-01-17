7308877e3eca5b9c5858b5b2c8df4b7a1f36eb03
Todd Kao <todd.kakao@gmail.com>
2025-01-17 12:02:28 -0500

    tunnel to /api/sentry

--- packages/widget/src/index.tsx
+++ packages/widget/src/index.tsx
@@ -16,6 +16,7 @@ import {
 
 init({
   dsn: "https://10ce608bdd1c68a13d3849d6b242333c@o4504768725909504.ingest.us.sentry.io/4508485201231872",
+  tunnel: "/api/sentry",
   defaultIntegrations: false,
   denyUrls: [/^https?:\/\/localhost:.*/],
   integrations: [
