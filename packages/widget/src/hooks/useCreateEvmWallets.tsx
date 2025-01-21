16e97b821032ea7a9a2c7f0931690b5ae88f350b
Nur Fikri <codingki@gmail.com>
2025-01-22 02:39:47 +0700

    disconnect hook

--- packages/widget/src/hooks/useCreateEvmWallets.tsx
+++ packages/widget/src/hooks/useCreateEvmWallets.tsx
@@ -6,7 +6,7 @@ import { useAtomValue, useSetAtom } from "jotai";
 import { useCallback } from "react";
 import { createPublicClient, http } from "viem";
 import { sei } from "viem/chains";
-import { useAccount, useConnect, useConnectors } from "wagmi";
+import { useAccount, useConnect, useConnectors, useDisconnect } from "wagmi";
 import { ChainType } from "@skip-go/client";
 import { walletConnectLogo } from "@/constants/wagmi";
 import { callbacksAtom } from "@/state/callbacks";
@@ -28,7 +28,7 @@ export const useCreateEvmWallets = () => {
   const { connectAsync } = useConnect();
   const connectors = useConnectors();
   const currentConnector = connectors.find((connector) => connector.id === currentEvmConnector?.id);
-
+  const { disconnectAsync } = useDisconnect();
   const mobile = isMobile();
 
   const createEvmWallets = useCallback(
@@ -58,7 +58,7 @@ export const useCreateEvmWallets = () => {
               connector,
               chainId: Number(chainID),
             });
-            await connector?.disconnect();
+            await disconnectAsync();
             setEvmWallet(undefined);
             return res.accounts[0];
           }
@@ -227,6 +227,7 @@ export const useCreateEvmWallets = () => {
       chainId,
       evmAddress,
       connectAsync,
+      disconnectAsync,
       setEvmWallet,
       currentConnector,
       chains,
