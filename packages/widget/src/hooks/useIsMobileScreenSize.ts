9924f52ad893146d543fe936c929d0f2df7449b6
Todd Kao <todd.kakao@gmail.com>
2025-01-23 16:20:00 -0500

    test using optional chaining on window

--- packages/widget/src/hooks/useIsMobileScreenSize.ts
+++ packages/widget/src/hooks/useIsMobileScreenSize.ts
@@ -4,21 +4,21 @@ export const MAX_MOBILE_SCREEN_WIDTH = 767;
 
 export const useIsMobileScreenSize = () => {
   const [isMobileScreenSize, setIsMobileScreenSize] = useState(
-    window.innerWidth <= MAX_MOBILE_SCREEN_WIDTH,
+    window?.innerWidth <= MAX_MOBILE_SCREEN_WIDTH,
   );
 
   useEffect(() => {
     const handleResize = () => {
-      const isMobileScreenSize = window.innerWidth <= MAX_MOBILE_SCREEN_WIDTH;
+      const isMobileScreenSize = window?.innerWidth <= MAX_MOBILE_SCREEN_WIDTH;
       setIsMobileScreenSize(isMobileScreenSize);
     };
 
     handleResize();
 
-    window.addEventListener("resize", handleResize);
+    window?.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener("resize", handleResize);
+      window?.removeEventListener("resize", handleResize);
     };
   }, []);
   return isMobileScreenSize;
