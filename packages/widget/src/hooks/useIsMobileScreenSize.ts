2e1fdb559e1ac627ae22403eafaea5e4d7bd9259
Todd Kao <todd.kakao@gmail.com>
2025-01-24 01:14:08 -0500

    Revert "test"
    This reverts commit 675eb0d1ff4f2b0ec4ee90b14fc85f2410397963.

--- packages/widget/src/hooks/useIsMobileScreenSize.ts
+++ packages/widget/src/hooks/useIsMobileScreenSize.ts
@@ -3,21 +3,22 @@ import { useState, useEffect } from "react";
 export const MAX_MOBILE_SCREEN_WIDTH = 767;
 
 export const useIsMobileScreenSize = () => {
-  const [isMobileScreenSize, setIsMobileScreenSize] = useState(false);
+  const [isMobileScreenSize, setIsMobileScreenSize] = useState(
+    window.innerWidth <= MAX_MOBILE_SCREEN_WIDTH,
+  );
 
   useEffect(() => {
-    if (window === undefined) return;
     const handleResize = () => {
-      const isMobileScreenSize = window?.innerWidth <= MAX_MOBILE_SCREEN_WIDTH;
+      const isMobileScreenSize = window.innerWidth <= MAX_MOBILE_SCREEN_WIDTH;
       setIsMobileScreenSize(isMobileScreenSize);
     };
 
     handleResize();
 
-    window?.addEventListener("resize", handleResize);
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      window?.removeEventListener("resize", handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
   return isMobileScreenSize;
