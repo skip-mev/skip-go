2ad7c669132bc1c421b8ad34494e90241d7db4c3
Todd Kao <todd.kakao@gmail.com>
2025-01-29 12:21:23 -0500

    test

--- packages/widget/src/widget/initSentry.ts
+++ packages/widget/src/widget/initSentry.ts
@@ -16,7 +16,7 @@ export const initSentry = () => {
   if (isSentryInitialized) return;
   init({
     dsn: "https://10ce608bdd1c68a13d3849d6b242333c@o4504768725909504.ingest.us.sentry.io/4508485201231872",
-    tunnel: "https://go.skip.build/api/sentry",
+    tunnel: "api/sentry",
     defaultIntegrations: false,
     denyUrls: [/^https?:\/\/localhost:.*/],
     integrations: [
