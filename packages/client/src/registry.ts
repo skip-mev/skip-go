294320ae0436bd566bde64609f63a649d3ff0b41
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2025-01-29 18:27:31 +0100

    refactor: imports

--- packages/client/src/registry.ts
+++ packages/client/src/registry.ts
@@ -1,12 +1,12 @@
-import { AccountParser, accountFromAny } from '@cosmjs/stargate';
+import { AccountParser, accountFromAny } from "@cosmjs/stargate";
 import { assert } from "@cosmjs/utils";
-import { StridePeriodicVestingAccount } from './stride';
-import { BaseAccount } from './codegen/cosmos/auth/v1beta1/auth';
-import { EthAccount } from '@injectivelabs/core-proto-ts/cjs/injective/types/v1beta1/account';
+import { StridePeriodicVestingAccount } from "./stride";
+import { BaseAccount } from "./codegen/cosmos/auth/v1beta1/auth";
+import { InjectiveTypesV1Beta1Account } from "@injectivelabs/core-proto-ts";
 
 export const accountParser: AccountParser = (acc) => {
   switch (acc.typeUrl) {
-    case "/stride.vesting.StridePeriodicVestingAccount":
+    case "/stride.vesting.StridePeriodicVestingAccount": {
       const baseAccount = StridePeriodicVestingAccount.decode(acc.value)
         .baseVestingAccount?.baseAccount;
       assert(baseAccount);
@@ -14,8 +14,11 @@ export const accountParser: AccountParser = (acc) => {
         typeUrl: "/cosmos.auth.v1beta1.BaseAccount",
         value: BaseAccount.encode(baseAccount).finish(),
       });
-    case "/injective.types.v1beta1.EthAccount":
-      const injAccount = EthAccount.decode(acc.value as Uint8Array);
+    }
+    case "/injective.types.v1beta1.EthAccount": {
+      const injAccount = InjectiveTypesV1Beta1Account.EthAccount.decode(
+        acc.value as Uint8Array,
+      );
       const baseInjAccount = injAccount.baseAccount!;
       const pubKey = baseInjAccount.pubKey;
 
@@ -23,16 +26,18 @@ export const accountParser: AccountParser = (acc) => {
         address: baseInjAccount.address,
         pubkey: pubKey
           ? {
-            type: "/injective.crypto.v1beta1.ethsecp256k1.PubKey",
-            value: Buffer.from(pubKey.value).toString("base64"),
-          }
+              type: "/injective.crypto.v1beta1.ethsecp256k1.PubKey",
+              value: Buffer.from(pubKey.value).toString("base64"),
+            }
           : null,
         accountNumber: Number(baseInjAccount.accountNumber),
         sequence: Number(baseInjAccount.sequence),
       };
-
-    case "/ethermint.types.v1.EthAccount":
-      const account = EthAccount.decode(acc.value as Uint8Array);
+    }
+    case "/ethermint.types.v1.EthAccount": {
+      const account = InjectiveTypesV1Beta1Account.EthAccount.decode(
+        acc.value as Uint8Array,
+      );
       const baseEthAccount = account.baseAccount!;
       const pubKeyEth = baseEthAccount.pubKey;
 
@@ -40,13 +45,14 @@ export const accountParser: AccountParser = (acc) => {
         address: baseEthAccount.address,
         pubkey: pubKeyEth
           ? {
-            type: "/ethermint.crypto.v1.ethsecp256k1.PubKey",
-            value: Buffer.from(pubKeyEth.value).toString("base64"),
-          }
+              type: "/ethermint.crypto.v1.ethsecp256k1.PubKey",
+              value: Buffer.from(pubKeyEth.value).toString("base64"),
+            }
           : null,
         accountNumber: Number(baseEthAccount.accountNumber),
         sequence: Number(baseEthAccount.sequence),
       };
+    }
     default:
       return accountFromAny(acc);
   }
