51622d1e5e9e44f4f12806030916eca92e7f0b1a
Todd Kao <toddkao@users.noreply.github.com>
2025-01-31 13:48:19 -0500

    Fix autoSetAddress for evm on mobile (#742)

--- packages/widget/src/hooks/useAutoSetAddress.ts
+++ packages/widget/src/hooks/useAutoSetAddress.ts
@@ -18,7 +18,6 @@ import NiceModal from "@ebay/nice-modal-react";
 import { Modals } from "@/modals/registerModals";
 import { ChainType } from "@skip-go/client";
 import { WalletSource } from "@/modals/SetAddressModal/SetAddressModal";
-import { isMobile } from "@/utils/os";
 
 export const useAutoSetAddress = () => {
   const [chainAddresses, setChainAddresses] = useAtom(chainAddressesAtom);
@@ -34,8 +33,6 @@ export const useAutoSetAddress = () => {
   const { createSolanaWallets } = useCreateSolanaWallets();
 
   const connectedAddress = useAtomValue(connectedAddressesAtom);
-
-  const mobile = isMobile();
 
   const signRequiredChains = useMemo(() => {
     if (!route?.operations) return;
@@ -168,9 +165,6 @@ export const useAutoSetAddress = () => {
             try {
               const wallets = createEvmWallets(chainID);
               const wallet = wallets.find((w) => w.walletName === sourceWallet.evm?.walletName);
-              if (wallet?.walletName === "walletConnect" && mobile) {
-                return;
-              }
               if (!wallet) {
                 if (!openModal) return;
                 showSetAddressModal();
@@ -221,7 +215,6 @@ export const useAutoSetAddress = () => {
       sourceWallet.evm?.walletName,
       createSolanaWallets,
       createEvmWallets,
-      mobile,
     ],
   );
 
