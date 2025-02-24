b9a15091cfaa26d7999abd9b1f508afa471a7696
Todd Kao <todd.kakao@gmail.com>
2025-02-24 11:41:56 -0500

    Fix eof error

--- packages/client/src/registry.ts
+++ packages/client/src/registry.ts
@@ -1,52 +1,61 @@
-import { AccountParser, accountFromAny } from '@cosmjs/stargate';
-import { assert } from "@cosmjs/utils";
-import { StridePeriodicVestingAccount } from './stride';
-import { BaseAccount } from './codegen/cosmos/auth/v1beta1/auth';
-import { EthAccount } from '@injectivelabs/core-proto-ts/cjs/injective/types/v1beta1/account';
+import { AccountParser, accountFromAny } from "@cosmjs/stargate";
+import { assertDefinedAndNotNull } from "@cosmjs/utils";
+import { StridePeriodicVestingAccount } from "./stride";
+import { EthAccount } from "@injectivelabs/core-proto-ts/cjs/injective/types/v1beta1/account";
+import { decodePubkey } from "@cosmjs/proto-signing";
 
 export const accountParser: AccountParser = (acc) => {
   switch (acc.typeUrl) {
-    case "/stride.vesting.StridePeriodicVestingAccount":
+    case "/stride.vesting.StridePeriodicVestingAccount": {
       const baseAccount = StridePeriodicVestingAccount.decode(acc.value)
         .baseVestingAccount?.baseAccount;
-      assert(baseAccount);
-      return accountFromAny({
-        typeUrl: "/cosmos.auth.v1beta1.BaseAccount",
-        value: BaseAccount.encode(baseAccount).finish(),
-      });
-    case "/injective.types.v1beta1.EthAccount":
+      assertDefinedAndNotNull(baseAccount);
+
+      return {
+        address: baseAccount.address,
+        pubkey: baseAccount.pubKey ? decodePubkey(baseAccount.pubKey) : null,
+        accountNumber: Number(baseAccount.accountNumber),
+        sequence: Number(baseAccount.sequence),
+      };
+    }
+    case "/injective.types.v1beta1.EthAccount": {
       const injAccount = EthAccount.decode(acc.value as Uint8Array);
-      const baseInjAccount = injAccount.baseAccount!;
+      const baseInjAccount = injAccount.baseAccount;
+      assertDefinedAndNotNull(baseInjAccount);
+
       const pubKey = baseInjAccount.pubKey;
 
       return {
         address: baseInjAccount.address,
         pubkey: pubKey
           ? {
-            type: "/injective.crypto.v1beta1.ethsecp256k1.PubKey",
-            value: Buffer.from(pubKey.value).toString("base64"),
-          }
+              type: "/injective.crypto.v1beta1.ethsecp256k1.PubKey",
+              value: Buffer.from(pubKey.value).toString("base64"),
+            }
           : null,
         accountNumber: Number(baseInjAccount.accountNumber),
         sequence: Number(baseInjAccount.sequence),
       };
-
-    case "/ethermint.types.v1.EthAccount":
+    }
+    case "/ethermint.types.v1.EthAccount": {
       const account = EthAccount.decode(acc.value as Uint8Array);
-      const baseEthAccount = account.baseAccount!;
+      const baseEthAccount = account.baseAccount;
+      assertDefinedAndNotNull(baseEthAccount);
+
       const pubKeyEth = baseEthAccount.pubKey;
 
       return {
         address: baseEthAccount.address,
         pubkey: pubKeyEth
           ? {
-            type: "/ethermint.crypto.v1.ethsecp256k1.PubKey",
-            value: Buffer.from(pubKeyEth.value).toString("base64"),
-          }
+              type: "/ethermint.crypto.v1.ethsecp256k1.PubKey",
+              value: Buffer.from(pubKeyEth.value).toString("base64"),
+            }
           : null,
         accountNumber: Number(baseEthAccount.accountNumber),
         sequence: Number(baseEthAccount.sequence),
       };
+    }
     default:
       return accountFromAny(acc);
   }
