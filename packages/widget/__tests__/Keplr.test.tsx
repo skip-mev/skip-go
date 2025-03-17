4f3c678ef431808864551bb14e1483158ff53d49
Todd Kao <todd.kakao@gmail.com>
2025-03-17 18:20:11 -0400

    test

--- packages/widget/__tests__/Keplr.test.tsx
+++ packages/widget/__tests__/Keplr.test.tsx
@@ -1,32 +1,24 @@
 import { test } from "./setup/fixtures";
-import { approveInKeplr } from "./setup/playwright";
-import {
-  connectDestination,
-  connectSource,
-  e2eTest,
-  expectPageLoaded,
-  fillAmount,
-  initKeplr,
-  selectAsset,
-} from "./setup/utils";
-// import { page } from "@vitest/browser/context";
+import { connectSource, expectPageLoaded, initKeplr, selectAsset } from "./setup/utils";
 
-test("Noble USDC -> Injective INJ", async ({ page }) => {
-  await initKeplr();
-  await expectPageLoaded(page);
+test.describe("Widget tests", () => {
+  test("Noble USDC -> Injective INJ", async ({ page }) => {
+    await initKeplr();
+    await expectPageLoaded(page);
 
-  await selectAsset({ page, asset: "USDC", chain: "Noble" });
-  await selectAsset({ page, asset: "INJ", chain: "Injective" });
+    await selectAsset({ page, asset: "USDC", chain: "Noble" });
+    await selectAsset({ page, asset: "INJ", chain: "Injective" });
 
-  await connectSource(page);
+    await connectSource(page);
 
-  await page.waitForTimeout(5_000);
+    await page.waitForTimeout(5_000);
 
-  await page.screenshot({
-    path: "__tests__/Widget/connect-keplr.png",
+    await page.screenshot({
+      path: "__tests__/Widget/connect-keplr.png",
+    });
+
+    // await fillAmount(page, "5");
+
+    // await e2eTest(page);
   });
-
-  // await fillAmount(page, "5");
-
-  // await e2eTest(page);
 });
