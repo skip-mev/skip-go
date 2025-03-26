1df5d4b424c7c688cb9df2a39222d7cd35d21874
Todd Kao <todd.kakao@gmail.com>
2025-03-26 13:29:44 -0400

    test

--- packages/widget/__tests__/Keplr.test.tsx
+++ packages/widget/__tests__/Keplr.test.tsx
@@ -1,13 +1,11 @@
-import { Page, test } from "@playwright/test";
+import { expect, Page, test } from "@playwright/test";
 import { approveInKeplr } from "./setup/playwright";
 import { selectAsset } from "./setup/utils";
 import { setupBrowserContext } from "./setup/keplr";
 
-test.describe.serial("Widget tests", async () => {
-  let page: Page;
-
+test.describe("Widget tests", async () => {
   test("Noble USDC -> Injective INJ", async () => {
-    page = await setupBrowserContext();
+    const page = await setupBrowserContext();
     await page.waitForTimeout(100);
     await page.screenshot({
       animations: "disabled",
@@ -30,6 +28,11 @@ test.describe.serial("Widget tests", async () => {
       path: "__tests__/Widget/both-assets-selected.png",
     });
 
+    await expect(page.getByRole("button", { name: "USDC logo USDC" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "on Noble" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "INJ logo INJ" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "on Injective" })).toBeVisible();
+
     await page.getByText("Connect Wallet").click();
     await page.getByText("Keplr").click();
 
@@ -45,14 +48,23 @@ test.describe.serial("Widget tests", async () => {
     await page.getByText("Swap").click();
     await page.getByText("Confirm").click();
     await approveInKeplr();
-    await page.getByText(/go again/i).click({ timeout: 300_000 });
+
+    await expect(page.getByText(/go again/i)).toBeVisible({ timeout: 300_000 });
   });
 
   test("Injective INJ -> Cosmoshub ATOM", async () => {
+    const page = await setupBrowserContext();
     await page.evaluate(() => window.localStorage.clear());
     await page.reload();
     await selectAsset({ page, asset: "INJ", chain: "Injective" });
     await selectAsset({ page, asset: "ATOM", chain: "Cosmos Hub" });
+
+    await expect(page.getByRole("button", { name: "INJ logo INJ" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "on Injective" })).toBeVisible();
+
+    await expect(page.getByRole("button", { name: "ATOM logo ATOM" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "on Cosmos Hub" })).toBeVisible();
+
     await page.getByText("Connect Wallet").click();
     await page.getByText("Keplr").click();
     const input = page.getByRole("textbox");
@@ -60,14 +72,23 @@ test.describe.serial("Widget tests", async () => {
     await page.getByText("Swap").click();
     await page.getByText("Confirm").click();
     await approveInKeplr();
-    await page.getByText(/go again/i).click({ timeout: 300_000 });
+
+    await expect(page.getByText(/go again/i)).toBeVisible({ timeout: 300_000 });
   });
 
   test("Cosmoshub ATOM -> Noble USDC", async () => {
+    const page = await setupBrowserContext();
     await page.evaluate(() => window.localStorage.clear());
     await page.reload();
     await selectAsset({ page, asset: "ATOM", chain: "Cosmos Hub" });
     await selectAsset({ page, asset: "USDC", chain: "Noble" });
+
+    await expect(page.getByRole("button", { name: "ATOM logo ATOM" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "on Cosmos Hub" })).toBeVisible();
+
+    await expect(page.getByRole("button", { name: "USDC logo USDC" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "on Noble" })).toBeVisible();
+
     await page.getByText("Connect Wallet").click();
     await page.getByText("Keplr").click();
     const input = page.getByRole("textbox");
@@ -75,6 +96,7 @@ test.describe.serial("Widget tests", async () => {
     await page.getByText("Swap").click();
     await page.getByText("Confirm").click();
     await approveInKeplr();
-    await page.getByText(/go again/i).click({ timeout: 300_000 });
+
+    await expect(page.getByText(/go again/i)).toBeVisible({ timeout: 300_000 });
   });
 });
