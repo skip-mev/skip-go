5127bbeeca0059b4886a3d3eb64e4447512681ef
plubber <51789398+ericHgorski@users.noreply.github.com>
2025-04-01 16:54:23 -0400

    Refactor client package to remove core-proto-ts dependency and streamline imports from sdk-ts

--- packages/client/src/registry.ts
+++ packages/client/src/registry.ts
@@ -1,8 +1,8 @@
 import { AccountParser, accountFromAny } from "@cosmjs/stargate";
 import { assertDefinedAndNotNull } from "@cosmjs/utils";
 import { StridePeriodicVestingAccount } from "./stride";
-import { EthAccount } from "@injectivelabs/core-proto-ts/cjs/injective/types/v1beta1/account";
 import { decodePubkey } from "@cosmjs/proto-signing";
+import { accountEthParser } from "@injectivelabs/sdk-ts";
 
 export const accountParser: AccountParser = (acc) => {
   switch (acc.typeUrl) {
@@ -18,44 +18,13 @@ export const accountParser: AccountParser = (acc) => {
         sequence: Number(baseAccount.sequence),
       };
     }
-    case "/injective.types.v1beta1.EthAccount": {
-      const injAccount = EthAccount.decode(acc.value as Uint8Array);
-      const baseInjAccount = injAccount.baseAccount;
-      assertDefinedAndNotNull(baseInjAccount);
-
-      const pubKey = baseInjAccount.pubKey;
-
-      return {
-        address: baseInjAccount.address,
-        pubkey: pubKey
-          ? {
-              type: "/injective.crypto.v1beta1.ethsecp256k1.PubKey",
-              value: Buffer.from(pubKey.value).toString("base64"),
-            }
-          : null,
-        accountNumber: Number(baseInjAccount.accountNumber),
-        sequence: Number(baseInjAccount.sequence),
-      };
-    }
-    case "/ethermint.types.v1.EthAccount": {
-      const account = EthAccount.decode(acc.value as Uint8Array);
-      const baseEthAccount = account.baseAccount;
-      assertDefinedAndNotNull(baseEthAccount);
-
-      const pubKeyEth = baseEthAccount.pubKey;
-
-      return {
-        address: baseEthAccount.address,
-        pubkey: pubKeyEth
-          ? {
-              type: "/ethermint.crypto.v1.ethsecp256k1.PubKey",
-              value: Buffer.from(pubKeyEth.value).toString("base64"),
-            }
-          : null,
-        accountNumber: Number(baseEthAccount.accountNumber),
-        sequence: Number(baseEthAccount.sequence),
-      };
-    }
+    case "/injective.types.v1beta1.EthAccount":
+      return accountEthParser(
+        acc,
+        "/injective.crypto.v1beta1.ethsecp256k1.PubKey",
+      );
+    case "/ethermint.types.v1.EthAccount":
+      return accountEthParser(acc, "/ethermint.crypto.v1.ethsecp256k1.PubKey");
     default:
       return accountFromAny(acc);
   }
