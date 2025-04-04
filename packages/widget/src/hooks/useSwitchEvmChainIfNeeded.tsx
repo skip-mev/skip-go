318f03c7c9b191bd9bc98db91afb9a8e82597dcc
Todd Kao <todd.kakao@gmail.com>
2025-04-04 13:30:35 -0400

    Fix evm disconnect, fix useSwitchChainIfNeeded

--- packages/widget/src/hooks/useSwitchEvmChainIfNeeded.tsx
+++ packages/widget/src/hooks/useSwitchEvmChainIfNeeded.tsx
@@ -1,24 +1,22 @@
-import { useCallback, useEffect, useMemo } from "react";
-import { useGetAccount } from "./useGetAccount";
+import { useCallback, useEffect } from "react";
 import { useAccount } from "wagmi";
 import { sourceAssetAtom } from "@/state/swapPage";
 import { useAtomValue } from "jotai";
 import { skipChainsAtom } from "@/state/skipClient";
+import { evmWalletAtom } from "@/state/wallets";
 
 export const useSwitchEvmChainIfNeeded = () => {
-  const getAccount = useGetAccount();
+  const evmWallet = useAtomValue(evmWalletAtom);
+
   const { chainId: evmChainId, connector } = useAccount();
   const sourceAsset = useAtomValue(sourceAssetAtom);
   const { data: chains } = useAtomValue(skipChainsAtom);
 
-  const evmAddress = useMemo(() => {
-    return evmChainId ? getAccount(String(evmChainId))?.address : undefined;
-  }, [evmChainId, getAccount]);
-  // Utility function to handle chain switching for EVM chains
   const switchToEvmChainIfNeeded = useCallback(
     (targetChainId?: string) => {
-      if (targetChainId && chains && evmAddress && connector) {
+      if (targetChainId && chains && evmWallet?.id && connector) {
         const isEvm = chains.find((c) => c.chainID === targetChainId)?.chainType === "evm";
+
         if (isEvm && targetChainId !== String(evmChainId)) {
           connector.switchChain?.({
             chainId: Number(targetChainId),
@@ -26,7 +24,7 @@ export const useSwitchEvmChainIfNeeded = () => {
         }
       }
     },
-    [chains, connector, evmAddress, evmChainId],
+    [chains, connector, evmChainId, evmWallet?.id],
   );
 
   // Effect to automatically switch chain when source asset changes (including direction reversal)
