02fbc77b30f40fc2eaefd739c7d4bfa9113c734b
Todd Kao <toddkao@users.noreply.github.com>
2025-04-07 17:03:41 -0400

    Go back to manually calling switchChain (#1012)

---
+++ packages/widget/src/hooks/useSwitchEvmChain.tsx
@@ -0,0 +1,24 @@
+import { useAccount } from "wagmi";
+import { useAtomValue } from "jotai";
+import { skipChainsAtom } from "@/state/skipClient";
+import { useCallback } from "react";
+
+export const useSwitchEvmChain = () => {
+  const { connector } = useAccount();
+  const { data: chains } = useAtomValue(skipChainsAtom);
+
+  const switchEvmChainId = useCallback(
+    (chainId?: string) => {
+      const isEvmChainId = chains?.find((c) => c.chainID === chainId)?.chainType === "evm";
+
+      if (isEvmChainId) {
+        connector?.switchChain?.({
+          chainId: Number(chainId),
+        });
+      }
+    },
+    [chains, connector],
+  );
+
+  return switchEvmChainId;
+};
