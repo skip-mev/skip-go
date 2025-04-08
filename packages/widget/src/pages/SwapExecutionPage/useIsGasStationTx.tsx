9224311b8fb6c88ab67843dc73d04b856e16a78f
plubber <51789398+ericHgorski@users.noreply.github.com>
2025-04-08 12:05:13 -0400

    refactor: enhance gas station transaction logic with balance checks and operation types

--- packages/widget/src/pages/SwapExecutionPage/useIsGasStationTx.tsx
+++ packages/widget/src/pages/SwapExecutionPage/useIsGasStationTx.tsx
@@ -3,17 +3,26 @@ import { useMemo } from "react";
 import { skipChainsAtom } from "@/state/skipClient";
 import { swapExecutionStateAtom } from "@/state/swapExecutionPage";
 import { GAS_STATION_CHAIN_IDS, ChainType } from "@skip-go/client";
+import { ClientOperation, OperationType } from "@/utils/clientType";
+import { useGetBalance } from "@/hooks/useGetBalance";
 
 export const useIsGasStationTx = () => {
   const { route } = useAtomValue(swapExecutionStateAtom);
   const { data: chains } = useAtomValue(skipChainsAtom);
+  const getBalance = useGetBalance();
 
   const isGasStationTx = useMemo(() => {
     if (!chains || !route) return false;
 
-    const destChain = chains.find((chain) => chain.chainID === route.sourceAssetChainID);
-    return destChain?.chainType === ChainType.EVM &&
-           GAS_STATION_CHAIN_IDS.includes(route.destAssetChainID);
+    const sourceChain = chains.find((chain) => chain.chainID === route.sourceAssetChainID);
+    const destChain = chains.find((chain) => chain.chainID === route.destAssetChainID);
+
+    const destChainFeeBalance = getBalance(destChain.chainID, destChain?.feeAssets[0]?.denom?.amount);
+    const hasGasFeeBalance = Number(destChainFeeBalance) > 0;
+    const isAxelarTransfer = route.operations.some((op: ClientOperation) => OperationType.axelarTransfer in op);
+
+    return sourceChain?.chainType === ChainType.EVM && !hasGasFeeBalance &&
+    !isAxelarTransfer && GAS_STATION_CHAIN_IDS.includes(destChain?.chainID);
   }, [chains, route]);
 
   return isGasStationTx;
