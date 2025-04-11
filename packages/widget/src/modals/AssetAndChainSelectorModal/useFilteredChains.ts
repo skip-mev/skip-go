44d814358041721b2c04938edd3431c04494f188
Todd Kao <todd.kakao@gmail.com>
2025-04-11 18:33:26 -0400

    add missing value in dep array

--- packages/widget/src/modals/AssetAndChainSelectorModal/useFilteredChains.ts
+++ packages/widget/src/modals/AssetAndChainSelectorModal/useFilteredChains.ts
@@ -181,6 +181,7 @@ export const useFilteredChains = ({
     cosmosWalletConnected,
     filter,
     filterOut,
+    filterOutUnlessUserHasBalance,
     getBalance,
     hideAssetsUnlessWalletTypeConnected,
     ibcEurekaHighlightedAssets,
