ebda8f546457d954ba9170d18c80d65d2409a670
Todd Kao <todd.kakao@gmail.com>
2025-04-24 21:16:36 -0400

    Update index.ts

--- packages/client/src/client-v2/index.ts
+++ packages/client/src/client-v2/index.ts
@@ -4,8 +4,12 @@ export { assetsFromSource } from "./api/getAssetsFromSource";
 export { balances } from "./api/getBalances";
 export { bridges } from "./api/getBridges";
 export { chains } from "./api/getChains";
-
+export { venues } from "./api/getVenues";
+export { ibcOriginAssets } from "./api/postIbcOriginAssets";
 export { messages } from "./api/postMessages";
+export { messagesDirect } from "./api/postMessagesDirect";
+export { recommendAssets } from "./api/postRecommendAssets";
+
 export { route } from "./api/postRoute";
 
 export { setClientOptions } from "./setClientOptions";
