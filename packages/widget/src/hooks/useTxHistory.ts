854c59a4364b2d053017b6835c83ae088b9894d6
Todd Kao <todd.kakao@gmail.com>
2025-04-25 12:07:02 -0400

    Fix useTxHistory returning completed if lastTxSuccess, instead should only return completed if isSettled and isSuccess are true

--- packages/widget/src/hooks/useTxHistory.ts
+++ packages/widget/src/hooks/useTxHistory.ts
@@ -65,9 +65,8 @@ export const useTxHistory = ({
     queryFn: () => {
       // Incomplete is when multiple transactions are required but not all txs are signed/tracked
       if (txs.length !== txsRequired) return "incomplete";
-      if (statusData?.lastTxStatus === "success") return "completed";
       if (isFetching && isPending) return "unconfirmed";
-      if (statusData?.isSuccess) return "completed";
+      if (statusData?.isSettled && statusData?.isSuccess) return "completed";
       if ((statusData?.isSettled && !statusData?.isSuccess) || !chainIdFound) return "failed";
       return "pending";
     },
