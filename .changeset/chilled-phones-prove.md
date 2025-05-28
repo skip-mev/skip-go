83b57bfe4f824362c522448de649abac14daf109
Todd Kao <todd.kakao@gmail.com>
2025-05-28 17:29:20 -0400

    Only pass specific fields to msgs api request

---
+++ .changeset/chilled-phones-prove.md
@@ -0,0 +1,5 @@
+---
+"@skip-go/client": patch
+---
+
+Only pass specific fields to msgs api request
--- packages/client/src/api/postMessages.ts
+++ packages/client/src/api/postMessages.ts
@@ -1,11 +1,30 @@
 import { api } from "../utils/generateApi";
 import type { ApiRequest, ApiResponse } from "../utils/generateApi";
 
-export const messages = api({
-  methodName: "msgs",
-  method: "post",
-  path: "/v2/fungible/msgs",
-});
+export const messages = async (request: MessagesRequest) => {
+  const messagesRequest = {
+    sourceAssetDenom: request.sourceAssetDenom,
+    sourceAssetChainId: request.sourceAssetChainId,
+    destAssetDenom: request.destAssetDenom,
+    destAssetChainId: request.destAssetChainId,
+    amountIn: request.amountIn,
+    amountOut: request.amountOut,
+    addressList: request.addressList,
+    operations: request.operations,
+    estimatedAmountOut: request.estimatedAmountOut,
+    slippageTolerancePercent: request.slippageTolerancePercent,
+    timeoutSeconds: request.timeoutSeconds,
+    postRouteHandler: request.postRouteHandler,
+    chainIdsToAffiliates: request.chainIdsToAffiliates,
+    enableGasWarnings: request.enableGasWarnings,
+  };
+
+  return api({
+    methodName: "msgs",
+    method: "post",
+    path: "/v2/fungible/msgs",
+  })(messagesRequest);
+};
 
 export type MessagesRequest = ApiRequest<"msgs">;
 export type MessagesResponse = ApiResponse<"msgs">;
