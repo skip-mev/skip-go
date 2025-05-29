e83cf118329c6c2e80ffff446bb6b43ee3564613
Todd Kao <todd.kakao@gmail.com>
2025-05-29 13:03:15 -0400

    fix import path

--- packages/client/src/api/postMessages.ts
+++ packages/client/src/api/postMessages.ts
@@ -1,6 +1,6 @@
 import { api } from "../utils/generateApi";
 import type { ApiRequest, ApiResponse } from "../utils/generateApi";
-import { filterMessagesRequest } from "../utils/getMessagesRequestFromRoute";
+import { filterMessagesRequest } from "../utils/filterMessagesRequest";
 
 export const messages = async (request: MessagesRequest) => {
   return api({
