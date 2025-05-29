69c5c98731c5a4f82167155eb4aaf3154eb3d1a5
Todd Kao <todd.kakao@gmail.com>
2025-05-29 12:09:51 -0400

    add filterMessagesRequest function

---
+++ packages/client/src/utils/filterMessagesRequest.ts
@@ -0,0 +1,22 @@
+import type { MessagesRequest } from "src/api/postMessages";
+
+export const filterMessagesRequest = (request: MessagesRequest) => {
+  const messagesRequest = {
+    sourceAssetDenom: request.sourceAssetDenom,
+    sourceAssetChainId: request.sourceAssetChainId,
+    destAssetDenom: request.destAssetDenom,
+    destAssetChainId: request.destAssetChainId,
+    amountIn: request.amountIn,
+    amountOut: request.amountOut,
+    addressList: request.addressList,
+    operations: request.operations,
+    estimatedAmountOut: request.estimatedAmountOut,
+    slippageTolerancePercent: request.slippageTolerancePercent,
+    timeoutSeconds: request.timeoutSeconds,
+    postRouteHandler: request.postRouteHandler,
+    chainIdsToAffiliates: request.chainIdsToAffiliates,
+    enableGasWarnings: request.enableGasWarnings,
+  };
+
+  return messagesRequest;
+}
