eb543462cc5065a89e9032dea2c421ea69fbc711
Todd Kao <todd.kakao@gmail.com>
2025-06-09 11:49:23 -0400

    update nuxtjs example

--- examples/nuxtjs/app.vue
+++ examples/nuxtjs/app.vue
@@ -7,11 +7,20 @@
 </template>
 
 <script setup>
-  const skipWidget = document.querySelector("skip-widget");
+import { onMounted } from 'vue';
+
+onMounted(async () => {
+  await customElements.whenDefined('skip-widget');
+
+  const skipWidget = document?.querySelector("skip-widget");
 
   if (skipWidget) {
+    skipWidget.brandColor = "lightgreen";
     skipWidget.onRouteUpdated = (route) => {
       console.log("route updated", route);
     };
+    // You can set other properties or callbacks here
+    // skipWidget.prop = value;
   }
+});
 </script>
--- examples/nuxtjs/plugins/skip-widget.client.ts
+++ examples/nuxtjs/plugins/skip-widget.client.ts
@@ -1,5 +1,7 @@
 import { defineNuxtPlugin } from '#app';
 
 export default defineNuxtPlugin(() => {
-  import('https://unpkg.com/@skip-go/widget-web-component/build/index.js') as any
+  import('@skip-go/widget-web-component')
+  // or replace with link below to use cdn link
+  // import('https://unpkg.com/@skip-go/widget-web-component/build/index.js') as any
 })
