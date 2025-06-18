ab9eac7b1b1efed641e5c8ff1aaea4e9dd495080
Abdul Malek <me@almk.dev>
2025-06-18 11:38:41 -0400

    update docs to specify base58 format

--- packages/client/src/types/swaggerTypes.ts
+++ packages/client/src/types/swaggerTypes.ts
@@ -2170,7 +2170,7 @@ export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDa
        * @default false
        */
       enable_gas_warnings?: boolean;
-      /** Alternative address to use for paying for fees, currently only for SVM source CCTP transfers */
+      /** Alternative address to use for paying for fees, currently only for SVM source CCTP transfers, in b58 format. */
       fee_payer_address?: string;
     },
     params: RequestParams = {},
@@ -2239,7 +2239,7 @@ export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDa
        * @default false
        */
       enable_gas_warnings?: boolean;
-      /** Alternative address to use for paying for fees, currently only for SVM source CCTP transfers */
+      /** Alternative address to use for paying for fees, currently only for SVM source CCTP transfers, in b58 format. */
       fee_payer_address?: string;
     },
     params: RequestParams = {},
