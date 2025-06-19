cfa5446b537501d0e33847440dd4e72573d1ff62
Todd Kao <todd.kakao@gmail.com>
2025-06-19 12:36:45 -0400

    Add cjs version at /cjs

--- packages/client/tsup.config.ts
+++ packages/client/tsup.config.ts
@@ -1,4 +1,4 @@
-import type { Options } from "tsup";
+import type { Format, Options } from "tsup";
 import { defineConfig } from "tsup";
 import { dependencies, peerDependencies } from "./package.json";
 import glob from "fast-glob";
@@ -17,43 +17,50 @@ export const autoExportFilesInDirectory = (path: string) => {
 const apiEntrypoints = autoExportFilesInDirectory("src/api/*");
 const publicFunctions = autoExportFilesInDirectory("src/public-functions/*");
 
-const defaultOptions: Options = {
+const sharedConfig = {
   cjsInterop: true,
-  clean: true,
-  dts: true,
-  format: ["esm"],
   minify: false,
   shims: true,
   splitting: true,
   bundle: true,
   treeshake: true,
   tsconfig: "./tsconfig.build.json",
+  clean: true,
+  entry: {
+    index: "src/index.ts",
+    ...apiEntrypoints,
+    ...publicFunctions,
+  },
+  external: [
+    ...Object.keys(dependencies || {}),
+    ...Object.keys(peerDependencies || {}),
+    /^@cosmjs\/.*/,
+    /^@injectivelabs\/.*/,
+    /^@protobufjs\/.*/,
+    /^@solana\/.*/,
+    "kujira.js",
+    "chain-registry",
+    "@initia/initia-registry",
+    "viem",
+    "long",
+    "protobufjs",
+  ],
 };
 
 export default defineConfig(async ({ watch }) => {
   return [
     {
-      ...defaultOptions,
+      ...sharedConfig,
+      dts: true,
       clean: !watch,
-      entry: {
-        index: "src/index.ts",
-        ...apiEntrypoints,
-        ...publicFunctions,
-      },
-      external: [
-        ...Object.keys(dependencies || {}),
-        ...Object.keys(peerDependencies || {}),
-        /^@cosmjs\/.*/,
-        /^@injectivelabs\/.*/,
-        /^@protobufjs\/.*/,
-        /^@solana\/.*/,
-        "kujira.js",
-        "chain-registry",
-        "@initia/initia-registry",
-        "viem",
-        "long",
-        "protobufjs",
-      ],
+      format: ["esm"],
+    },
+    {
+      ...sharedConfig,
+      dts: false,
+      clean: !watch,
+      format: ["cjs"],
+      outDir: "dist/cjs",
     },
   ];
 });
