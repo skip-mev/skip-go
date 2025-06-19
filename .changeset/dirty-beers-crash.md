3041b459cbf8a023d464cc677f9bdcd1a0caef53
Todd Kao <toddkao@users.noreply.github.com>
2025-06-19 14:17:14 -0400

    Revert indexedDb back to localStorage, add transactionHistoryVersion (#1393)

---
+++ .changeset/dirty-beers-crash.md
@@ -0,0 +1,5 @@
+---
+"@skip-go/widget": patch
+---
+
+Revert indexedDb back to localStorage, add transactionHistoryVersion
--- packages/widget/src/widget/Widget.tsx
+++ packages/widget/src/widget/Widget.tsx
@@ -23,7 +23,7 @@ import { rootIdAtom } from "@/state/skipClient";
 import packageJson from "../../package.json";
 import { IbcEurekaHighlightedAssets } from "@/state/ibcEurekaHighlightedAssets";
 import { ChainFilter } from "@/state/filters";
-import { migrateHistoryFromLocalStorageToIndexedDB } from "@/utils/migrateOldLocalStorageValues";
+import { migrateOldLocalStorageValues } from "@/utils/migrateOldLocalStorageValues";
 import { EVMProvider } from "@/providers/EVMProvider";
 import { CosmosProvider } from "@/providers/CosmosProvider";
 import { SolanaProvider } from "@/providers/SolanaProvider";
@@ -104,7 +104,7 @@ export const queryClient = new QueryClient();
 
 export const jotaiStore: ReturnType<typeof createStore> = createStore();
 
-migrateHistoryFromLocalStorageToIndexedDB();
+migrateOldLocalStorageValues();
 
 export const Widget = (props: WidgetProps) => {
   return (
