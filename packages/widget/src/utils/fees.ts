9beee7cc9c069daf159b05b1f530723c20fcd603
Todd Kao <toddkao@users.noreply.github.com>
2025-06-20 12:30:25 -0400

    Refactor fee code to take data from estimated_fees (#1405)

---
+++ packages/widget/src/utils/fees.ts
@@ -0,0 +1,82 @@
+import { formatUSD } from "@/utils/intl";
+import { convertTokenAmountToHumanReadableAmount } from "./crypto";
+import { FeeType, Fee, RouteResponse, BridgeType } from "@skip-go/client";
+
+export type FeeDetail = {
+  assetAmount: number;
+  formattedAssetAmount: string;
+  formattedUsdAmount?: string;
+};
+export type LabeledFee = {
+  label: string;
+  fee: FeeDetail;
+};
+
+const getFeeDetail = (estimatedFee: Fee): FeeDetail => {
+  const humanReadableAmount = convertTokenAmountToHumanReadableAmount(
+    estimatedFee.amount ?? 0,
+    estimatedFee.originAsset.decimals,
+  );
+  const totalUsd = Number(estimatedFee.usdAmount);
+
+  return {
+    assetAmount: Number(humanReadableAmount),
+    formattedAssetAmount: `${humanReadableAmount} ${estimatedFee.originAsset.symbol}`,
+    formattedUsdAmount: formatUSD(totalUsd.toString()),
+  };
+};
+
+const BRIDGE_ID_TO_LABEL_MAP: Record<BridgeType, string> = {
+  [BridgeType.IBC]: "IBC",
+  [BridgeType.AXELAR]: "Axelar",
+  [BridgeType.HYPERLANE]: "Hyperlane",
+  [BridgeType.GO_FAST]: "Go-Fast",
+  [BridgeType.OPINIT]: "OpInit",
+  [BridgeType.CCTP]: "CCTP",
+  [BridgeType.EUREKA]: "IBC Eureka",
+  [BridgeType.STARGATE]: "Stargate",
+  [BridgeType.LAYER_ZERO]: "Layer Zero",
+};
+
+export function getFeeList(route: RouteResponse): LabeledFee[] {
+  if (!route.estimatedFees || route.estimatedFees.length === 0) {
+    return [];
+  }
+
+  return route.estimatedFees.map((fee) => {
+    if (fee.feeType === FeeType.SMART_RELAY) {
+      return {
+        label: "Smart Relay Fee",
+        fee: getFeeDetail(fee),
+      };
+    }
+
+    const label = fee.bridgeId
+      ? `${BRIDGE_ID_TO_LABEL_MAP[fee.bridgeId]} Bridging Fee`
+      : "Bridging Fee";
+    const feeDetail = getFeeDetail(fee);
+
+    return {
+      label,
+      fee: feeDetail,
+    };
+  });
+}
+
+export function getTotalFees(fees: LabeledFee[]): FeeDetail | undefined {
+  if (!fees.length) return;
+
+  const totalAsset = fees.reduce((s, { fee }) => s + fee.assetAmount, 0);
+  const totalUsd = fees.reduce(
+    (s, { fee }) =>
+      s + (fee.formattedUsdAmount ? Number(fee.formattedUsdAmount.replace(/[^0-9.-]/g, "")) : 0),
+    0,
+  );
+  const symbol = fees[0].fee.formattedAssetAmount.split(" ")[1];
+
+  return {
+    assetAmount: totalAsset,
+    formattedAssetAmount: `${totalAsset.toFixed(6)} ${symbol}`,
+    formattedUsdAmount: totalUsd > 0 ? formatUSD(totalUsd.toString()) : undefined,
+  };
+}
