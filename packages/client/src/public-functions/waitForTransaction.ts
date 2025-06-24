cadc8ccd2bf324832db149c6b8d2f9563fbee431
Todd Kao <toddkao@users.noreply.github.com>
2025-06-24 11:51:44 -0400

    Add calling track back for svm/evm (#1413)

--- packages/client/src/public-functions/waitForTransaction.ts
+++ packages/client/src/public-functions/waitForTransaction.ts
@@ -1,15 +1,34 @@
+import type { TransactionCallbacks } from "src/types/callbacks";
 import { transactionStatus } from "../api/postTransactionStatus";
 import { wait } from "../utils/timer";
+import { trackTransaction } from "src/api/postTrackTransaction";
+import type { TxResult } from "src/types/client-types";
 
-export type WaitForTransactionProps = {
-  chainId: string;
-  txHash: string;
+export type WaitForTransactionProps = TxResult & {
+  onTransactionTracked?: TransactionCallbacks["onTransactionTracked"];
 };
 
 export const waitForTransaction = async ({
   chainId,
   txHash,
+  explorerLink,
+  onTransactionTracked,
+  ...trackTxPollingOptions
 }: WaitForTransactionProps) => {
+  try {
+    if (!explorerLink) {
+      const response = await trackTransaction({
+        chainId: chainId,
+        txHash: txHash,
+        ...trackTxPollingOptions,
+      });
+      explorerLink = response.explorerLink;
+    }
+    await onTransactionTracked?.({ txHash, chainId, explorerLink });
+  } catch (error) {
+    console.warn(`track failed for txHash:${txHash}, chainId: ${chainId}`);
+  }
+
   // eslint-disable-next-line no-constant-condition
   while (true) {
     const txStatusResponse = await transactionStatus({
