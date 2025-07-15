ddd7bf67e7df05e0fae912c8284611e1125827df
Todd Kao <todd.kakao@gmail.com>
2025-07-15 18:01:06 -0400

    use proxy urls for amplitude

--- packages/widget/src/widget/initAmplitude.ts
+++ packages/widget/src/widget/initAmplitude.ts
@@ -1,18 +1,23 @@
 import { init, add } from "@amplitude/analytics-browser";
 import { version } from "../../package.json";
-import { sessionReplayPlugin } from '@amplitude/plugin-session-replay-browser';
+import { sessionReplayPlugin } from "@amplitude/plugin-session-replay-browser";
 
-let isAmplitudeInitialized = false;
+const isAmplitudeInitialized = false;
 
 export const initAmplitude = () => {
   if (isAmplitudeInitialized) return;
-  
+
+  const serverUrl = "https://go.skip.build/api/amplitude";
+
   init("14616a575f32087cf0403ab8f3ea3ce0", {
     appVersion: version,
+    serverUrl: `${serverUrl}/httpapi`,
   });
 
   const plugin = sessionReplayPlugin({
     sampleRate: 1,
+    trackServerUrl: `${serverUrl}/upload`,
+    configServerUrl: `${serverUrl}/config`,
   });
   add(plugin);
 };
