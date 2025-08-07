8c3be3dbe190424c885c181d69024d6950012fb4
Nur Fikri <codingki@gmail.com>
2025-08-07 15:27:02 +0700

    revert

--- packages/widget/src/pages/SwapPage/SwapPage.tsx
+++ packages/widget/src/pages/SwapPage/SwapPage.tsx
@@ -243,7 +243,7 @@ export const SwapPage = () => {
     if (!sourceAccount?.address && !isInvertingSwap) {
       return (
         <MainButton
-          label="Connects Wallet"
+          label="Connect Wallet"
           icon={ICONS.plus}
           onClick={() => {
             track("swap page: connect wallet button - clicked");
