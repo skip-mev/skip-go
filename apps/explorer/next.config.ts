702791673abef86db1cdff8c4fff02ed45abc10d
Todd Kao <todd.kakao@gmail.com>
2025-08-08 03:11:39 -0400

    Update next.config.ts

--- apps/explorer/next.config.ts
+++ apps/explorer/next.config.ts
@@ -1,5 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  webpack: (config) => {
+
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      type: 'asset/resource',
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
