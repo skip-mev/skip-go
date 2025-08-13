6b934767c16ebde04de147b74177158072b98eb3
Todd Kao <todd.kakao@gmail.com>
2025-08-13 10:09:19 -0400

    remove @tanstack/react-query-devtools

--- apps/explorer/src/components/QueryProvider.tsx
+++ apps/explorer/src/components/QueryProvider.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
 
 export function QueryProvider({ children }: { children: React.ReactNode }) {
@@ -23,7 +22,6 @@ export function QueryProvider({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
