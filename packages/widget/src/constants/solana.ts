693b1c200cbdaba3e38d8b57a361ff3984533afe
Nur Fikri <codingki@gmail.com>
2025-08-15 08:54:57 +0700

    chore: add changeset for solana provider

--- packages/widget/src/constants/solana.ts
+++ packages/widget/src/constants/solana.ts
@@ -1,13 +1,18 @@
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { LedgerWalletAdapter } from "@solana/wallet-adapter-ledger";
-import { WalletConnectWalletAdapter } from "@walletconnect/solana-adapter";
 
-export const solanaWallets = [
-  new LedgerWalletAdapter(),
-  new WalletConnectWalletAdapter({
-    network: WalletAdapterNetwork.Mainnet,
-    options: {
-      projectId: "ff1b9e9bd6329cfb07642bd7f4d11a8c",
-    },
-  }),
-];
+export const solanaWallets = [new LedgerWalletAdapter()];
+
+export const addWalletConnectWalletAdapter = async () => {
+  const { WalletConnectWalletAdapter } = await import("@walletconnect/solana-adapter");
+  if (!solanaWallets.some((wallet) => wallet.name === "WalletConnect")) {
+    solanaWallets.push(
+      new WalletConnectWalletAdapter({
+        network: WalletAdapterNetwork.Mainnet,
+        options: {
+          projectId: "ff1b9e9bd6329cfb07642bd7f4d11a8c",
+        },
+      })
+    );
+  }
+};
--- packages/widget/src/providers/SolanaProvider.tsx
+++ packages/widget/src/providers/SolanaProvider.tsx
@@ -1,10 +1,23 @@
-import { solanaWallets } from "@/constants/solana";
 import { WalletProvider } from "@solana/wallet-adapter-react";
+import { useQuery } from "@tanstack/react-query";
+
+import { addWalletConnectWalletAdapter, solanaWallets } from "@/constants/solana";
 
 export const SolanaProvider = ({ children }: { children: React.ReactNode }) => {
+  const query = useQuery({
+    queryKey: ["solanaWallets"],
+    queryFn: async () => {
+      await addWalletConnectWalletAdapter();
+      return [...solanaWallets];
+    },
+    initialData: solanaWallets,
+    staleTime: Infinity,
+    enabled: typeof window !== "undefined",
+  });
+
   return (
     <WalletProvider
-      wallets={solanaWallets}
+      wallets={query.data ?? solanaWallets}
       autoConnect={true}
       localStorageKey="skip-go-widget-solana-wallet"
     >
