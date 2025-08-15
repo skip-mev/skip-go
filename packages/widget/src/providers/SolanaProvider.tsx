3ba0d3889da9e31c232dc5a7dfcd4084057759e0
Nur Fikri <codingki@gmail.com>
2025-08-15 09:37:04 +0700

    fix dynamically add walletconnect to solana provider

--- packages/widget/src/providers/SolanaProvider.tsx
+++ packages/widget/src/providers/SolanaProvider.tsx
@@ -1,16 +1,28 @@
 import { WalletProvider } from "@solana/wallet-adapter-react";
 import { useQuery } from "@tanstack/react-query";
 
-import { addWalletConnectWalletAdapter, solanaWallets } from "@/constants/solana";
+import { solanaWallets } from "@/constants/solana";
+import { WalletAdapter, WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 
 export const SolanaProvider = ({ children }: { children: React.ReactNode }) => {
   const query = useQuery({
     queryKey: ["solanaWallets"],
     queryFn: async () => {
-      await addWalletConnectWalletAdapter();
-      return [...solanaWallets];
+      const wallets: WalletAdapter[] = [...solanaWallets];
+      // Ensure the WalletConnect wallet adapter is added only once
+      const { WalletConnectWalletAdapter } = await import("@walletconnect/solana-adapter");
+      if (!wallets.some((wallet) => wallet.name === "WalletConnect")) {
+        wallets.push(
+          new WalletConnectWalletAdapter({
+            network: WalletAdapterNetwork.Mainnet,
+            options: {
+              projectId: "ff1b9e9bd6329cfb07642bd7f4d11a8c",
+            },
+          }),
+        );
+      }
+      return wallets;
     },
-    initialData: solanaWallets,
     staleTime: Infinity,
     enabled: typeof window !== "undefined",
   });
