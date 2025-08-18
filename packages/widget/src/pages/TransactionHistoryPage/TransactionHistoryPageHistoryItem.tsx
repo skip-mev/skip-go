7ebaf21d4c69bb0669e6a18705f8fb726dcc5327
Todd Kao <todd.kakao@gmail.com>
2025-08-18 16:09:45 -0400

    comment out base64Encoded url params in widget

--- packages/widget/src/pages/TransactionHistoryPage/TransactionHistoryPageHistoryItem.tsx
+++ packages/widget/src/pages/TransactionHistoryPage/TransactionHistoryPageHistoryItem.tsx
@@ -50,20 +50,20 @@ export const TransactionHistoryPageHistoryItem = forwardRef<
       txHistoryItem,
     });
 
-    const explorerData = useMemo(() => {
-      const jsonString = JSON.stringify({
-        route: historyItem?.route,
-        userAddresses: historyItem?.userAddresses,
-        transactionDetails: historyItem?.transactionDetails,
-        status: historyItem?.status,
-      });
+    // const explorerData = useMemo(() => {
+    //   const jsonString = JSON.stringify({
+    //     route: historyItem?.route,
+    //     userAddresses: historyItem?.userAddresses,
+    //     transactionDetails: historyItem?.transactionDetails,
+    //     status: historyItem?.status,
+    //   });
 
-      const base64Encoded = Buffer.from(jsonString).toString("base64");
+    //   const base64Encoded = Buffer.from(jsonString).toString("base64");
 
-      return base64Encoded;
-    }, [historyItem]);
+    //   return base64Encoded;
+    // }, [historyItem]);
 
-    console.log(historyItem?.transactionDetails, explorerData.length, explorerData);
+    // console.log(historyItem?.transactionDetails, explorerData.length, explorerData);
 
     const removeTransactionHistoryItem = useSetAtom(removeTransactionHistoryItemAtom);
 
