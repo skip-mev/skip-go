3d602f4a6ced85baab904a91692af630996f0208
Todd Kao <todd.kakao@gmail.com>
2025-08-19 13:26:09 -0400

    Create ErrorCard.tsx

---
+++ apps/explorer/src/components/ErrorCard.tsx
@@ -0,0 +1,10 @@
+import { Container } from "@/components/Container"
+import { SmallText } from "@/components/Typography"
+
+export const ErrorCard = () => {
+  return (
+    <Container>
+      <SmallText>Error</SmallText>
+    </Container>
+  )
+}
