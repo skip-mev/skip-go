a528ae29070d829d7185b5e472b7a59b6eefc004
Todd Kao <todd.kakao@gmail.com>
2025-08-20 14:33:47 -0400

    add ability to press enter in txHash input and search if chainId is already selected

--- apps/explorer/src/app/page.tsx
+++ apps/explorer/src/app/page.tsx
@@ -300,6 +300,11 @@ export default function Home() {
             size={isTop ? "small" : "normal"}
             value={txHash || ""}
             onChange={(v) => setTxHash(v)}
+            onSearch={() => {
+              if (txHash && chainId) {
+                onSearch();
+              }
+            }}
             openModal={() => {
               NiceModal.show(Modals.AssetAndChainSelectorModal, {
                 context: "source",
