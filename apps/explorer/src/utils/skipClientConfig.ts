62274080f3fb2fe32bb8cf563c1ca448f605963d
JunyupHong <junyup.hong@gmail.com>
2026-07-23 18:51:49 +0900

    change SKIP_API_URL for test

--- apps/explorer/src/utils/skipClientConfig.ts
+++ apps/explorer/src/utils/skipClientConfig.ts
@@ -1,7 +1,6 @@
-import { setApiOptions } from "@skip-go/client";
-import { defaultSkipClientConfig } from "@/state/skipClient";
+import { setApiOptions } from '@skip-go/client';
+import { defaultSkipClientConfig } from '@/state/skipClient';
 
-export const SKIP_API_URL =
-  process.env.NEXT_PUBLIC_API_URL || defaultSkipClientConfig.apiUrl;
+export const SKIP_API_URL = 'https://api.skip-dev.cosmoslabs.kr';
 
 setApiOptions({ apiUrl: SKIP_API_URL });
